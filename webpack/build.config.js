db8dab14955c65baa2c474b2c9626774428d7d06
Jennifer Stern <jstern@instructure.com>
2016-03-21 16:36:12 -0500

    Fix incorrect paths to node_modules in transpiled JS
    Change-Id: I8744ec39a2d676f010f3f1f48872c90e6da74d51
    Reviewed-on: https://gerrit.instructure.com/75018
    Tested-by: Jenkins
    Reviewed-by: Sterling Cobb <sterling@instructure.com>
    Reviewed-by: Ryan Shaw <ryan@instructure.com>
    Product-Review: Ryan Shaw <ryan@instructure.com>
    QA-Review: Ryan Shaw <ryan@instructure.com>

--- webpack/build.config.js
+++ webpack/build.config.js
@@ -1,12 +1,24 @@
 /* eslint no-var: 0 */
 'use strict'
 
-var config = require('./util/config')
+var opts = require('./util/config')
 
-module.exports = require('./util/generate-config')({
+var config = require('./util/generate-config')({
   output: {
     libraryTarget: 'commonjs2',
-    path: config.buildPath + '/lib'
+    path: opts.buildPath + '/lib'
   },
   externals: require('./util/externals')
 }, process.env.NODE_ENV, process.env.MINIFY)
+
+// need to override default resolve config for the build
+// so that the babel transform-runtime plugin generates the correct paths
+// this can be fixed when the cli is extracted from this repo
+config.resolve = {
+  root: opts.rootPath,
+  modulesDirectories: [
+    'node_modules'
+  ]
+}
+
+module.exports = config
