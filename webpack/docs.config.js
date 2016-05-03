c470f49774396befabfd05b690a4282a6dd0370d
Jennifer Stern <jstern@instructure.com>
2016-05-02 21:17:00 -0500

    Fix production build
    https://github.com/webpack/webpack/issues/300
    
    Change-Id: Ie260b0132a8f8536d2409cd3661c8f218d739304
    Reviewed-on: https://gerrit.instructure.com/78555
    Tested-by: Jenkins
    Reviewed-by: Colleen Palmer <colleen@instructure.com>
    Product-Review: Colleen Palmer <colleen@instructure.com>
    QA-Review: Colleen Palmer <colleen@instructure.com>

--- webpack/docs.config.js
+++ webpack/docs.config.js
@@ -7,7 +7,7 @@ var webpack = require('webpack')
 var config = require('./util/config')
 
 var entry = {
-  'example': path.join(config.docsAppPath, 'lib/example.js'),
+  'example': [ path.join(config.docsAppPath, 'lib/example.js') ],
   'docs': [
     path.join(config.docsAppPath, 'lib/index.js')
   ],
@@ -17,7 +17,7 @@ var entry = {
   'vendor': ['react', 'react-dom', 'react-addons-css-transition-group']
 }
 
-entry[config.library.packageName] = path.join(config.rootPath, config.library.main)
+entry[config.library.packageName] = [ path.join(config.rootPath, config.library.main) ]
 
 module.exports = require('./util/generate-config')({
   devtool: (process.env.NODE_ENV === 'production') ? null : 'cheap-module-eval-source-map',
