55513e6d13f48312ea6912d2a08b09ebf4a2459b
Jennifer Stern <jstern@instructure.com>
2016-07-07 14:40:13 -0500

    Fix table-flip script
    nvm has to be loaded first
    
    Change-Id: If9324a155a75de3979994ea7f97ea2a17e19121a
    Reviewed-on: https://gerrit.instructure.com/84566
    Reviewed-by: Pam Hiett <phiett@instructure.com>
    Product-Review: Pam Hiett <phiett@instructure.com>
    QA-Review: Pam Hiett <phiett@instructure.com>
    Tested-by: Jennifer Stern <jstern@instructure.com>

--- table-flip.sh
+++ table-flip.sh
@@ -2,4 +2,11 @@
 
 echo '(╯°□°）╯︵ ┻━┻'
 
-nvm use && rm -rf node_modules && npm cache clean && npm install
+echo "-- install node"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" && nvm install && nvm use
+
+echo "-- installing dependencies"
+rm -rf node_modules
+npm cache clean
+npm install
