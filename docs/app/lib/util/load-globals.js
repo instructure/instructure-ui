299e36089f7993efffecdbd7d47c8cfe6fc9ddbf
Jennifer Stern <jstern@instructure.com>
2016-07-15 13:53:03 -0500

    Add icon and icon-inverse Button variants
    Change-Id: I71f69d13f7c460a98a914ea8ac7adac62793fb08
    Reviewed-on: https://gerrit.instructure.com/85466
    Tested-by: Jenkins
    Reviewed-by: Andy Brown <abrown@instructure.com>
    Reviewed-by: Stephen Jensen <sejensen@instructure.com>
    Product-Review: Jennifer Stern <jstern@instructure.com>
    QA-Review: Jennifer Stern <jstern@instructure.com>

--- docs/app/lib/util/load-globals.js
+++ docs/app/lib/util/load-globals.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import placeholderImage from 'mocks/util/placeholder-image'
+import { IconComposeSolid } from 'instructure-icons'
 import avatarImage from 'mocks/images/placeholder-avatar.png'
 import faker from 'faker'
 
 // These need to be globals to render examples
 global.placeholderImage = placeholderImage
+global.PlaceholderIcon = IconComposeSolid
 global.avatarImage = avatarImage
 global.React = React
 global.ReactDOM = ReactDOM
