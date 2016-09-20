19f2a3ed2c158f268a60e636a16d607e4169cb9c
Jennifer Stern <jstern@instructure.com>
2016-09-20 17:10:51 -0500

    Remove react-axe to fix doc scroll/jump on interact w/ component example
    This only happens in dev mode, but it's pretty annoying, so we'll pull
    react-axe until we can find a better solution.
    
    fixes INSTUI-113
    
    Change-Id: I19958764c2239ec7f6cdd557c88502fb0cdf3a32
    Reviewed-on: https://gerrit.instructure.com/90906
    Tested-by: Jenkins
    Reviewed-by: Stephen Jensen <sejensen@instructure.com>
    Product-Review: Stephen Jensen <sejensen@instructure.com>
    QA-Review: Jennifer Stern <jstern@instructure.com>

--- docs/app/lib/example.js
+++ docs/app/lib/example.js
@@ -4,14 +4,4 @@ import ComponentExample from './components/ComponentExample'
 
 import 'babel-polyfill'
 
-if (process.env.NODE_ENV !== 'production') {
-  const axe = require('react-axe')
-
-  axe(React, ReactDOM, 1000, {
-    rules: [
-      { id: 'radiogroup', enabled: false } /* https://github.com/dequelabs/axe-core/issues/176 */
-    ]
-  })
-}
-
 ReactDOM.render(<ComponentExample />, document.getElementById('example'))
