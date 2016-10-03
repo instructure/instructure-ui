c581c7769581092d8cefea30abc4cbf5077e26fa
Stephen Jensen <sejensen@instructure.com>
2016-10-03 17:42:06 -0600

    INSTUI button design update
    Closes: INSTUI-100
    
    This commit revises INSTUI buttons according to the design
    mockup here -> http://www.screencast.com/t/RcFyggk4
    
    It includes the addition of four button variants ('light',
    'ghost', 'circle-primary', and 'circle-danger') along with
    an updated focus state for all buttons excluding the 'link'
    and 'icon' variants.
    
    Test Plan:
    - The new button variants are all displayed in the button
      documentation
    
    - In all cases, the aesthetic of each button should now be
      consistent with the mockup provided for each button state
      (active, hover, focus, disabled)
    
    Change-Id: Ie239e909092d8e2b522d0eb7c5886ce4e406c687
    Reviewed-on: https://gerrit.instructure.com/91927
    Tested-by: Jenkins
    Reviewed-by: Jennifer Stern <jstern@instructure.com>
    QA-Review: Pierce Arner <pierce@instructure.com>
    Product-Review: Kyle Follett <kfollett@instructure.com>

--- docs/app/lib/util/load-globals.js
+++ docs/app/lib/util/load-globals.js
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import placeholderImage from 'mocks/util/placeholder-image'
 import IconComposeSolid from 'instructure-icons/react/Solid/IconComposeSolid'
+import IconPlusLine from 'instructure-icons/react/Line/IconPlusLine'
+import IconXLine from 'instructure-icons/react/Line/IconXLine'
 import avatarImage from 'mocks/images/placeholder-avatar.png'
 import lorem from 'lorem-ipsum'
 
 // These need to be globals to render examples
 global.placeholderImage = placeholderImage
 global.PlaceholderIcon = IconComposeSolid
+global.IconPlus = IconPlusLine
+global.IconX = IconXLine
 global.avatarImage = avatarImage
 global.React = React
 global.ReactDOM = ReactDOM
