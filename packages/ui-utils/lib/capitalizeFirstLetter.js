cb56606c0bad143991444e998df345e6d2729098
Jennifer Stern <jstern@instructure.com>
2017-10-09 11:04:40 -0500

    fix(build): ignore lib directories
    Fixes n/a
    
    Test plan:
    lib directories should be ignored by git
    
    Change-Id: Ided017b6e4bf85d8a48315344efa2de8f51e2f69
    Reviewed-on: https://gerrit.instructure.com/128978
    Tested-by: Jenkins
    Reviewed-by: Matt Zabriskie <mzabriskie@instructure.com>
    Product-Review: Jennifer Stern <jstern@instructure.com>
    QA-Review: Jennifer Stern <jstern@instructure.com>

--- packages/ui-utils/lib/capitalizeFirstLetter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = capitalizeFirstLetter;
-function capitalizeFirstLetter(word) {
-  return word ? word.charAt(0).toUpperCase() + word.slice(1) : word;
-}
