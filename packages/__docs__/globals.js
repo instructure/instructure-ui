0a3f30c37742f4c312479e912a2c78547d7ef3dd
Jennifer Stern <jstern@instructure.com>
2017-10-20 14:42:50 -0500

    fix(ui-docs-client): Codepen links are missing some globals
    Test plan:
    Verify that codepen links work on gh-pages
    
    Change-Id: I5812113547f54c7af404848a348e53f08f66f480
    Reviewed-on: https://gerrit.instructure.com/130465
    Reviewed-by: Pam Hiett <phiett@instructure.com>
    Product-Review: Pam Hiett <phiett@instructure.com>
    QA-Review: Jennifer Stern <jstern@instructure.com>
    Tested-by: Jenkins
    Reviewed-on: https://gerrit.instructure.com/134153
    Reviewed-by: Chris Hart <chart@instructure.com>
    Product-Review: Chris Hart <chart@instructure.com>
    QA-Review: Stephen Jensen <sejensen@instructure.com>

--- packages/__docs__/globals.js
+++ packages/__docs__/globals.js
@@ -5,9 +5,11 @@ import IconPlusSolid from 'instructure-icons/lib/Solid/IconPlusSolid'
 import IconXSolid from 'instructure-icons/lib/Solid/IconXSolid'
 import moment from 'moment'
 import 'moment/min/locales'
+import lorem from 'lorem-ipsum'
 
 import avatarImage from './placeholder-avatar.png'
 import iconExample from './heart_lg.svg'
+import placeholderImage from './placeholder-image'
 
 import * as UICore from '../ui-core/src'
 
@@ -17,7 +19,30 @@ const globals = Object.assign({
   IconX: IconXSolid,
   moment,
   avatarImage,
-  iconExample
+  iconExample,
+  lorem: {
+    sentence () {
+      return lorem({
+        count: 1,
+        units: 'sentences'
+      })
+    },
+    paragraph () {
+      return lorem({
+        count: 1,
+        units: 'paragraphs'
+      })
+    },
+    paragraphs (count) {
+      return lorem({
+        count: count || Math.floor(Math.random() * 10),
+        units: 'paragraphs'
+      })
+    }
+  },
+  placeholderImage,
+  React,
+  ReactDOM
 }, UICore)
 
 Object.keys(globals).forEach((key) => {
