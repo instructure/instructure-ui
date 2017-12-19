74fa7eb8d3b638b3bf0104bac8e6e25a3dbc149f
Jennifer Stern <jstern@instructure.com>
2017-12-18 18:05:17 -0600

    fix(scripts): Add babel and postcss config to package template
    Test plan:
    Running `yarn generate:package` should include the babel and postcss
    config
    
    Change-Id: Ie5eb5e63f710e51b97e7959f733016608f8f4428
    Reviewed-on: https://gerrit.instructure.com/136065
    Tested-by: Jenkins
    Reviewed-by: Stephen Jensen <sejensen@instructure.com>
    Product-Review: Stephen Jensen <sejensen@instructure.com>
    QA-Review: Jennifer Stern <jstern@instructure.com>

--- scripts/templates/package/babelrc.js
+++ scripts/templates/package/babelrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   // eslint-disable-next-line import/no-extraneous-dependencies
   presets: [[ require('@instructure/ui-presets/babel'), {
+    themeable: !process.env.DEBUG,
     coverage: Boolean(process.env.COVERAGE),
     esModules: Boolean(process.env.ES_MODULES)
   }]]
