6bb66bc65bb109476dd4b39cf964f5eb5e5ac231
Ken Meleta <kmeleta@instructure.com>
2018-02-16 13:41:09 -0700

    fix(Tag): Disabled Tags should not be keyboard focusable
    Fixes INSTUI-960
    
    Test plan:
    - Create a disabled Tag or view example in the docs
    - Try to tab to the disabled Tag
    - Ensure Tag doesn't receive focus
    
    Change-Id: Id2dd107e8cbcb3b773902c19da828cca7ff1483b
    Reviewed-on: https://gerrit.instructure.com/141256
    Tested-by: Jenkins
    Reviewed-by: Matt Zabriskie <mzabriskie@instructure.com>
    Product-Review: Stephen Jensen <sejensen@instructure.com>
    QA-Review: Dan Sasaki <dsasaki@instructure.com>

--- packages/ui-elements/src/components/Tag/index.js
+++ packages/ui-elements/src/components/Tag/index.js
@@ -130,6 +130,7 @@ class Tag extends Component {
         onClick={(onClick) ? this.handleClick : null}
         aria-disabled={(onClick && disabled) ? 'true' : null}
         display={null}
+        disabled={disabled}
         title={title || ((typeof text === 'string') ? text : null)}
       >
         <span className={styles.text}>
