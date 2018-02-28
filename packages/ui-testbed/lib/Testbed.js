fa7526491600bff60d4721a27bb33689d9fc932d
Jennifer Stern <jstern@instructure.com>
2018-02-28 15:20:25 -0700

    fix(ui-testbed): update CSS to disable transitions
    Test plan:
    Unit tests should pass
    
    Change-Id: Ic5358ac1a683579d37c8874f0f3f26a37f5760c8
    Reviewed-on: https://gerrit.instructure.com/142251
    Tested-by: Jenkins
    Reviewed-by: Ken Meleta <kmeleta@instructure.com>
    Product-Review: Ken Meleta <kmeleta@instructure.com>
    QA-Review: Jennifer Stern <jstern@instructure.com>

--- packages/ui-testbed/lib/Testbed.js
+++ packages/ui-testbed/lib/Testbed.js
@@ -146,10 +146,23 @@ Testbed.init = () => {
   sheet.inject()
   sheet.insert(`
     .no-transition * {
-      transform: none !important;
-      transition: none !important;
       transition-property: none !important;
+      -o-transition-property: none !important;
+      -moz-transition-property: none !important;
+      -ms-transition-property: none !important;
+      -webkit-transition-property: none !important;
+
+      transform: none !important;
+      -o-transform: none !important;
+      -moz-transform: none !important;
+      -ms-transform: none !important;
+      -webkit-transform: none !important;
+
       animation: none !important;
+      -o-animation: none !important;
+      -moz-animation: none !important;
+      -ms-animation: none !important;
+      -webkit-animation: none !important;
     }
   `)
 
