30fe57b781ef37a7458ecee3cd4506c5240f34fe
Ken McGrady <kmcgrady@instructure.com>
2018-04-03 12:54:00 -0700

    fix(media-capture): Update the FileSave submit button to prevent default
    Test plan:
    * Open up the Media Capture Test
    * Record a Web Cam Video
    * Click the save button
    * Verify the web page does not reload
    
    Change-Id: I45fe816b1d6fdf2bc52a25d194dc1633db65a608

--- packages/media-capture/src/components/MediaCapture/FileSave/index.js
+++ packages/media-capture/src/components/MediaCapture/FileSave/index.js
@@ -21,17 +21,14 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import Button from '@instructure/ui-buttons/lib/components/Button'
-import TextInput from '@instructure/ui-forms/lib/components/TextInput'
-import ScreenReaderContent from '@instructure/ui-a11y/lib/components/ScreenReaderContent'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Button from "@instructure/ui-buttons/lib/components/Button";
+import TextInput from "@instructure/ui-forms/lib/components/TextInput";
+import ScreenReaderContent from "@instructure/ui-a11y/lib/components/ScreenReaderContent";
 
-import {
-  SAVING
-} from '../../../constants/CaptureStates'
-import { translate } from '../../../constants/translated/translations'
-
+import { SAVING } from "../../../constants/CaptureStates";
+import { translate } from "../../../constants/translated/translations";
 
 /**
 ---
@@ -45,46 +42,56 @@ export default class FileSave extends Component {
     actions: PropTypes.shape({
       saveClicked: PropTypes.func.isRequired
     }).isRequired
-  }
+  };
 
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.state = {
       fileName: this.props.fileName
-    }
+    };
   }
 
   onChange = () => {
     this.setState({
       fileName: this.input.value
-    })
-  }
+    });
+  };
 
-  saveClicked = () => {
-    this.props.actions.saveClicked(this.state.fileName.trim())
-  }
+  saveClicked = e => {
+    e.preventDefault();
+    this.props.actions.saveClicked(this.state.fileName.trim());
+  };
 
-  render () {
+  render() {
     return (
-      <div style={{ display: 'flex', width: '100%' }}>
-        <TextInput
-          size="small"
-          label={<ScreenReaderContent>{translate('SR_FILE_INPUT')}</ScreenReaderContent>}
-          placeholder={this.props.fileName}
-          onChange={this.onChange}
-          inputRef={(e) => { this.input = e }}
-        />
-        <Button
-          onClick={this.saveClicked}
-          disabled={this.props.captureState === SAVING}
-          variant="primary"
-          size="small"
-          margin="0 0 0 x-small"
-        >
-          { translate('SAVE') }
-        </Button>
-      </div>
-    )
+      <form>
+        <div style={{ display: "flex", width: "100%" }}>
+          <TextInput
+            size="small"
+            label={
+              <ScreenReaderContent>
+                {translate("SR_FILE_INPUT")}
+              </ScreenReaderContent>
+            }
+            placeholder={this.props.fileName}
+            onChange={this.onChange}
+            inputRef={e => {
+              this.input = e;
+            }}
+          />
+          <Button
+            type="submit"
+            onClick={this.saveClicked}
+            disabled={this.props.captureState === SAVING}
+            variant="primary"
+            size="small"
+            margin="0 0 0 x-small"
+          >
+            {translate("SAVE")}
+          </Button>
+        </div>
+      </form>
+    );
   }
 }
