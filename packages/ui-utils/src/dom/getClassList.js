d388c044c162e07a5754ed24b4a80c21bfda249f
Ryan Shaw <ryan@instructure.com>
2018-10-23 11:42:45 -0600

    perf(ui-utils): simplify getClassList since all browsers we support have classList API
    IE 11 at least has the classList API. you still need to shim it for svg
    elements but once you do, you can count on it being there.
    see: https://caniuse.com/#search=classList
    
    TEST PLAN:
    - automated tests should pass and be a tiny bit faster
    - there should be no changes in the Transition component examples
    
    Change-Id: I22b76148ba0d563ac6ea52160dcdf121595a62bd
    Reviewed-on: https://gerrit.instructure.com/169422
    Tested-by: Jenkins
    QA-Review: Daniel Sasaki <dsasaki@instructure.com>
    Reviewed-by: Steve Jensen <sejensen@instructure.com>
    Product-Review: Steve Jensen <sejensen@instructure.com>

--- packages/ui-utils/src/dom/getClassList.js
+++ packages/ui-utils/src/dom/getClassList.js
@@ -26,6 +26,13 @@ import findDOMNode from './findDOMNode'
 
 let classListShimmed = false
 
+const apiForEmptyNode = {
+  toArray: () => [],
+  contains: () => false,
+  add: () => {},
+  remove: () => {}
+}
+
 /**
  * ---
  * category: utilities/DOM
@@ -42,60 +49,32 @@ let classListShimmed = false
 export default function getClassList (element) {
   const node = findDOMNode(element)
 
-  return {
+  if (!node) return apiForEmptyNode
+
+  const classListApi = {
     toArray () {
-      if (node && node.classList) {
-        return [...node.classList]
-      } else if (node && node.className) {
-        return node.className.split(/[ ,]+/)
-      } else {
-        return []
-      }
-    },
-
-    contains (className) {
-      return node && hasClass(node, className)
-    },
-
-    add (className) {
       shimClassListForIE()
-
-      if (node && node.classList) {
-        node.classList.add(className)
-      } else if (node && !hasClass(node)) {
-        node.className = `${node.className} ${className}`
-      }
-    },
-
-    remove (className) {
-      shimClassListForIE()
-
-      if (node && node.classList) {
-        node.classList.remove(className)
-      } else if (node) {
-        node.className = node.className // eslint-disable-line no-param-reassign
-          .replace(new RegExp(`(^|\\s)${className}(?:\\s|$)`, 'g'), '$1')
-          .replace(/\s+/g, ' ')
-          .replace(/^\s*|\s*$/g, '')
-      }
+      return [...node.classList]
     }
   }
-}
 
-function hasClass (node, className) {
-  shimClassListForIE()
-  if (node.classList) {
-    return !!className && node.classList.contains(className)
-  } else {
-    return ` ${node.className} `.indexOf(` ${className} `) !== -1
-  }
+  ;['contains', 'add', 'remove'].forEach(method => {
+    classListApi[method] = (className) => {
+      shimClassListForIE()
+      return node.classList[method](className)
+    }
+  })
+
+  return classListApi
 }
 
 function shimClassListForIE () { // IE 11 doesn't support classList on SVG elements
   /* istanbul ignore if */
-  if (!classListShimmed && !('classList' in document.createElementNS('http://www.w3.org/2000/svg', 'g'))) {
-    const descr = Object.getOwnPropertyDescriptor(HTMLElement.prototype, 'classList')
-    Object.defineProperty(SVGElement.prototype, 'classList', descr)
+  if (!classListShimmed) {
+    if (!('classList' in document.createElementNS('http://www.w3.org/2000/svg', 'g'))) {
+      const descr = Object.getOwnPropertyDescriptor(HTMLElement.prototype, 'classList')
+      Object.defineProperty(SVGElement.prototype, 'classList', descr)
+    }
     classListShimmed = true
   }
 }
