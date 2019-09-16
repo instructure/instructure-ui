b97f3fe355e06227bd0250a69b8f8db39e290568
Ryan Shaw <ryan@instructure.com>
2019-09-16 16:29:33 -0600

    fix(ui-utils): export isEdge from the root of @instructure/ui-utils
    in my commit where I recently added this I forgot to export it from 
    the index.js file
    
    TEST PLAN:
    * set transformImports:false in your babel config
    * run webpack
    * you should not see:
    "export 'isEdge' was not found in '@instructure/ui-utils'
    
    Change-Id: If4fd3d28737e1d00ffe31538d7f1ae762da44422
    Reviewed-on: https://gerrit.instructure.com/209762
    Tested-by: Jenkins
    QA-Review: Daniel Sasaki <dsasaki@instructure.com>
    Visual-Regression-Test: Steve Jensen <sejensen@instructure.com>
    Reviewed-by: Steve Jensen <sejensen@instructure.com>
    Product-Review: Steve Jensen <sejensen@instructure.com>

--- packages/ui-utils/src/index.js
+++ packages/ui-utils/src/index.js
@@ -22,6 +22,7 @@
  * SOFTWARE.
  */
 export { Browser } from './Browser'
+export { isEdge } from './isEdge'
 export { isIE11 } from './isIE11'
 export { capitalizeFirstLetter } from './capitalizeFirstLetter'
 export { cloneArray } from './cloneArray'
