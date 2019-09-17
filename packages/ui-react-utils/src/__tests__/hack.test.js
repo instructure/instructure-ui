1351477261951e7944beef515eeb9e5703f09aa9
Ken Meleta <kmeleta@instructure.com>
2019-09-17 14:22:49 -0600

    feat(ui-react-utils): add hack decorator
    refs: INSTUI-2267
    
    This commit adds a @hack decorator similar to
    @experimental. It is only applicable to props.
    
    TEST PLAN:
    - add a new prop to any component
    - add the hack decorator to the component and
    pass it the new prop name @hack(['myProp'])
    - use the prop in a docs example and verify
    the hack prop warning fires in the console
    
    Change-Id: I4255a23c8fadabfdae6eb05e0f7beb293947376e
    Reviewed-on: https://gerrit.instructure.com/209930
    Tested-by: Jenkins
    Reviewed-by: Steve Jensen <sejensen@instructure.com>
    Product-Review: Pam Hiett <phiett@instructure.com>
    QA-Review: Daniel Sasaki <dsasaki@instructure.com>
    Visual-Regression-Test: Ken Meleta <kmeleta@instructure.com>

---
+++ packages/ui-react-utils/src/__tests__/hack.test.js
@@ -0,0 +1,70 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 - present Instructure, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import { expect, mount, spy } from '@instructure/ui-test-utils'
+
+import { hack } from '../hack'
+
+class TestComponent extends Component {
+  static propTypes = {
+    bar: PropTypes.string,
+    qux: PropTypes.string
+  }
+
+  static defaultProps = {
+    bar: undefined,
+    qux: 'Hello'
+  }
+
+  render () {
+    return <div>{this.props.qux} {this.props.bar}</div>
+  }
+}
+
+describe('@hack', async () => {
+  describe('hack props', async () => {
+    const HackComponent = hack(['bar'])(TestComponent)
+
+    it('should warn when using an hack prop', async () => {
+      const consoleWarn = spy(console, 'warn')
+      await mount(<HackComponent bar="Jane" />)
+      await expect(consoleWarn)
+        .to.have.been.calledWithMatch([
+          'Warning: [TestComponent] ',
+          'The `bar` prop is a temporary hack and will be removed in a future release.'
+        ].join(''))
+    })
+
+    it('should not output a warning using a non-hack prop', async () => {
+      const consoleWarn = spy(console, 'warn')
+
+      await mount(<HackComponent qux="Jane" />)
+
+      await expect(consoleWarn).to.not.have.been.called()
+    })
+  })
+})
---
+++ packages/ui-react-utils/src/hack.js
@@ -0,0 +1,87 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 - present Instructure, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+import { decorator } from '@instructure/ui-decorator'
+
+import { warn } from '@instructure/console/macro'
+
+/**
+* ---
+* category: utilities/react
+* ---
+* Flag React component props as hack props.
+* Warnings will display in the console when hack props are used.
+*
+* ```js
+*  class Example extends Component {
+*    static propTypes = {
+*      currentProp: PropTypes.func
+*    }
+*  }
+*  export default hack(['hackProp'])(Example)
+* ```
+*
+* @module hack
+* @param {array} hackProps
+* @param {string} message
+* @return {function} React component flagged as having hack props
+*/
+const hack = decorator((ComposedComponent, hackProps, message) => {
+  class HackComponent extends ComposedComponent {}
+
+  if (process.env.NODE_ENV !== 'production') {
+    HackComponent.prototype.componentDidMount = function () {
+      if (hackProps) {
+        warnHackProps(ComposedComponent.displayName, this.props, hackProps, message)
+      }
+
+      if (ComposedComponent.prototype.componentDidMount) {
+        ComposedComponent.prototype.componentDidMount.call(this)
+      }
+    }
+
+    HackComponent.prototype.componentWillReceiveProps = function (nextProps, nextContext) {
+      if (hackProps) {
+        warnHackProps(ComposedComponent.displayName, nextProps, hackProps, message)
+      }
+
+      if (ComposedComponent.prototype.componentWillReceiveProps) {
+        ComposedComponent.prototype.componentWillReceiveProps.call(this, nextProps, nextContext)
+      }
+    }
+  }
+
+  return HackComponent
+})
+
+function warnHackProps (displayName, props, hackProps, message = '') {
+  hackProps.forEach((hackProp) => {
+    warn(
+      (typeof props[hackProp] === 'undefined'),
+      `[${displayName}] The \`${hackProp}\` prop is a temporary hack and will be removed in a future release. ${message}`
+    )
+  })
+}
+
+export default hack
+export { hack }
