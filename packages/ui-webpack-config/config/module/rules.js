983aef7265acceb628b2660911de6e5392d50fe5
Jim Simon <jsimon@instructure.com>
2020-05-08 13:09:53 -0400

    feat(ui-scripts): Add flags to ui-test to opt out of linting and coverage
    Change-Id: Ic9a0b70abafdba93995d3acd4f9549e45f78e5b7
    Reviewed-on: https://gerrit.instructure.com/c/instructure-ui/+/236702
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Steve Jensen <sejensen@instructure.com>
    QA-Review: Steve Jensen <sejensen@instructure.com>
    Product-Review: Steve Jensen <sejensen@instructure.com>
    Visual-Regression-Test: Steve Jensen <sejensen@instructure.com>
    Reviewed-on: https://gerrit.instructure.com/c/instructure-ui/+/240577
    Reviewed-by: Chris Guerrero <cguerrero@instructure.com>

--- packages/ui-webpack-config/config/module/rules.js
+++ packages/ui-webpack-config/config/module/rules.js
@@ -24,6 +24,7 @@
 const path = require('path')
 const ENV = process.env.NODE_ENV || 'production'
 const DEBUG = process.env.DEBUG || ENV === 'development'
+const noLint = process.argv.includes('--no-lint')
 const DISABLE_LINTER_FAILURE_ON_WARNING = (process.env.DISABLE_LINTER_FAILURE_ON_WARNING === '1') || false
 const exclude = [ /node_modules/, /\/lib\//, /\/es\// ]
 
@@ -34,21 +35,7 @@ const babelLoader = {
   }
 }
 
-module.exports = [
-  {
-    enforce: 'pre',
-    test: /\.js?$/,
-    exclude,
-    loader: 'eslint-loader',
-    options: {
-      failOnWarning: Boolean(!DEBUG && !DISABLE_LINTER_FAILURE_ON_WARNING),
-      emitError: Boolean(!DEBUG),
-      emitWarning: Boolean(DEBUG),
-      failOnError: Boolean(!DEBUG),
-      quiet: true,
-      ignorePath: path.join(process.cwd(), '.eslintignore')
-    }
-  },
+const rules = [
   {
     test: /\.js$/,
     exclude: [...exclude, /\.examples\.js$/],
@@ -122,3 +109,22 @@ module.exports = [
     }
   }
 ]
+
+if (!noLint) {
+  rules.unshift({
+    enforce: 'pre',
+    test: /\.js?$/,
+    exclude,
+    loader: 'eslint-loader',
+    options: {
+      failOnWarning: Boolean(!DEBUG && !DISABLE_LINTER_FAILURE_ON_WARNING),
+      emitError: Boolean(!DEBUG),
+      emitWarning: Boolean(DEBUG),
+      failOnError: Boolean(!DEBUG),
+      quiet: true,
+      ignorePath: path.join(process.cwd(), '.eslintignore')
+    }
+  })
+}
+
+module.exports = rules
