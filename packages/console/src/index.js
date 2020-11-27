8d37b5d58535044bd8c0b67c035fe01ceec9f30c
Matyas Forian-Szabo <matyas.szabo@instructure.com>
2020-11-27 20:24:34 +0100

    chore(console,ui-testable): update syntax to be compatible with node 12+
    set Node version to 10+`
    tested in Node v10 and 15

--- packages/console/src/index.js
+++ packages/console/src/index.js
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-const React = require('react')
+import React from 'react'
 
 let loggedInitialDeprecationWarning = false
 
@@ -64,13 +64,11 @@ function logDeprecated(condition, message, ...args) {
     )
   }
 }
-
-exports.error = (...args) => logMessage('error', true, ...args)
-exports.warn = (...args) => logMessage('warn', true, ...args)
-exports.warnDeprecated = (...args) => logDeprecated(...args)
-exports.info = (...args) => console.info(...args)
-exports.assert = (...args) => console.assert(...args)
-exports.debug = (...args) => console.debug(...args)
-exports.log = (...args) => console.log(...args)
-
+export let error = (...args) => logMessage('error', true, ...args)
+export let warn = (...args) => logMessage('warn', true, ...args)
+export let warnDeprecated = (...args) => logDeprecated(...args)
+export let info = (...args) => console.info(...args)
+export let assert = (...args) => console.assert(...args)
+export let debug = (...args) => console.debug(...args)
+export let log = (...args) => console.log(...args)
 /* eslint-enable no-console */
