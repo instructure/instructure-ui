6b4fbbec7e546b2c39ba37ba26f60069b871b653
Eszter Szabo <eszter.szabo@instructure.com>
2021-01-04 16:45:43 +0100

    refactor(ui-table): table colHeader themeable to emotion migration

---
+++ packages/ui-table/src/Table/ColHeader/styles.js
@@ -0,0 +1,85 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 - present Instructure, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import generateComponentTheme from './theme'
+
+/**
+ * Generates the style object from the theme and provided additional information
+ * @param  {Object} theme The actual theme object.
+ * @param  {Object} themeOverride User provided overrides of the default theme mapping.
+ * @param  {Object} props the props of the component, the style is applied to
+ * @param  {Object} state the state of the component, the style is applied to
+ * @return {Object} The final style object, which will be used in the component
+ */
+const generateStyle = (theme, themeOverride, props, state) => {
+  const componentTheme = generateComponentTheme(theme, themeOverride)
+
+  const { onRequestSort, textAlign } = props
+
+  const headerStyle = {
+    color: componentTheme.color,
+    fontSize: componentTheme.fontSize,
+    fontFamily: componentTheme.fontFamily,
+    fontWeight: 'bold',
+    lineHeight: componentTheme.lineHeight,
+    padding: componentTheme.padding
+  }
+
+  const buttonTextAlignVariants = {
+    start: { flexDirection: 'row' },
+    center: { justifyContent: 'center' },
+    end: { flexDirection: 'row-reverse' }
+  }
+
+  return {
+    colHeader: {
+      label: 'colHeader',
+      background: componentTheme.background,
+      verticalAlign: 'middle',
+      boxSizing: 'border-box',
+      ...(!onRequestSort && headerStyle),
+      textAlign
+    },
+    button: {
+      label: 'colHeader__button',
+      ...headerStyle,
+      display: 'flex',
+      alignItems: 'center',
+      boxSizing: 'border-box',
+      border: 'none',
+      width: '100%',
+      height: '100%',
+      background: 'transparent',
+      cursor: 'pointer',
+      outline: 'none',
+      '&::-moz-focus-inner': { border: 0 },
+      '&:focus': {
+        outline: `${componentTheme.focusOutlineWidth} ${componentTheme.focusOutlineStyle} ${componentTheme.focusOutlineColor}`
+      },
+      ...buttonTextAlignVariants[textAlign]
+    }
+  }
+}
+
+export default generateStyle
--- packages/ui-table/src/Table/ColHeader/theme.js
+++ packages/ui-table/src/Table/ColHeader/theme.js
@@ -22,22 +22,40 @@
  * SOFTWARE.
  */
 
-export default function generator({ typography, colors, borders, spacing }) {
+/**
+ * Generates the theme object for the component from the theme and provided additional information
+ * @param  {Object} theme The actual theme object.
+ * @param  {Object} themeOverride User provided overrides of the default theme mapping.
+ * @return {Object} The final theme object with the overrides and component variables
+ */
+const generateComponentTheme = (theme, themeOverride = {}) => {
+  const { typography, colors, borders, spacing, key: themeName } = theme
+
+  const themeSpecificStyle = {}
+
+  const componentVariables = {
+    fontSize: typography?.fontSizeMedium,
+    fontFamily: typography?.fontFamily,
+    fontWeight: typography?.fontWeightNormal,
+
+    color: colors?.textDarkest,
+    background: colors?.backgroundLightest,
+
+    borderColor: colors?.borderMedium,
+
+    lineHeight: typography?.lineHeightCondensed,
+    padding: `${spacing?.xSmall} ${spacing?.small}`,
+
+    focusOutlineColor: colors?.borderBrand,
+    focusOutlineWidth: borders?.widthMedium,
+    focusOutlineStyle: borders?.style
+  }
+
   return {
-    fontSize: typography.fontSizeMedium,
-    fontFamily: typography.fontFamily,
-    fontWeight: typography.fontWeightNormal,
-
-    color: colors.textDarkest,
-    background: colors.backgroundLightest,
-
-    borderColor: colors.borderMedium,
-
-    lineHeight: typography.lineHeightCondensed,
-    padding: `${spacing.xSmall} ${spacing.small}`,
-
-    focusOutlineColor: colors.borderBrand,
-    focusOutlineWidth: borders.widthMedium,
-    focusOutlineStyle: borders.style
+    ...componentVariables,
+    ...themeSpecificStyle[themeName],
+    ...themeOverride
   }
 }
+
+export default generateComponentTheme
