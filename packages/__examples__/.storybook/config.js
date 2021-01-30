c5c9516ea8df032cfc0e1ba25f018412b8962f0c
Viktor Ohad <viktor.ohad@instructure.com>
2021-01-30 14:20:43 +0100

    chore(ui-scripts): add VRT pipeline

--- packages/__examples__/.storybook/config.js
+++ packages/__examples__/.storybook/config.js
@@ -24,7 +24,6 @@
 
 // eslint-disable-next-line import/no-unresolved
 import '@instructure/ui-polyfill-loader!'
-import 'storybook-chromatic'
 
 import { create } from '@storybook/theming'
 import {
