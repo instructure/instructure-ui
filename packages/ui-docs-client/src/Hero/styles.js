69d20a941030db690d27db12c09c7845fa10b2a2
Eszter Szabo <eszter.szabo@instructure.com>
2021-02-02 16:07:29 +0100

    refactor(ui-docs-client): migrate Hero from themeable to emotion
    Closes: INSTUI-2779

--- packages/ui-docs-client/src/Hero/styles.js
+++ packages/ui-docs-client/src/Hero/styles.js
@@ -35,8 +35,8 @@
 const generateStyle = (componentTheme, props, state) => {
   const commonStyles = {
     position: 'absolute',
-    top: '0',
-    insetInlineStart: '0',
+    top: 0,
+    insetInlineStart: 0,
     width: '100%',
     height: '100%'
   }
--- packages/ui-docs-client/src/Hero/theme.js
+++ packages/ui-docs-client/src/Hero/theme.js
@@ -22,8 +22,26 @@
  * SOFTWARE.
  */
 
-export default function ({ colors }) {
+/**
+ * Generates the theme object for the component from the theme and provided additional information
+ * @param  {Object} theme The actual theme object.
+ * @param  {Object} themeOverride User provided overrides of the default theme mapping.
+ * @return {Object} The final theme object with the overrides and component variables
+ */
+const generateComponentTheme = (theme, themeOverride = {}) => {
+  const { colors, key: themeName } = theme
+
+  const themeSpecificStyle = {}
+
+  const componentVariables = {
+    backgroundColor: colors?.backgroundBrand
+  }
+
   return {
-    backgroundColor: colors.backgroundBrand
+    ...componentVariables,
+    ...themeSpecificStyle[themeName],
+    ...themeOverride
   }
 }
+
+export default generateComponentTheme
