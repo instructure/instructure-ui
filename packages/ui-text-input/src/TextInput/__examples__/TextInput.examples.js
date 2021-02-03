5b5a52a1e1e79bc9f93443e817551cd2503d9a23
Eszter Szabo <eszter.szabo@instructure.com>
2021-02-03 15:33:15 +0100

    refactor(ui-text-input): removed deprecated `label` prop
    BREAKING CHANGE:
    Removed deprecated `label` prop, use `renderLabel` instead.

--- packages/ui-text-input/src/TextInput/__examples__/TextInput.examples.js
+++ packages/ui-text-input/src/TextInput/__examples__/TextInput.examples.js
@@ -68,7 +68,7 @@ export default {
       layout: 'stacked'
     }
   },
-  excludeProps: ['required', 'readOnly', 'disabled', 'label', 'inline'],
+  excludeProps: ['required', 'readOnly', 'disabled', 'inline'],
   filter: (props) => {
     return (
       (props.layout === 'inline' && props.inline === true && !props.width) ||
