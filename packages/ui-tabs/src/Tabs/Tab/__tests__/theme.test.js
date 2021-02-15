d081a509571f293908134e15c8fa652b441d906c
Matyas Forian-Szabo <matyas.szabo@instructure.com>
2021-02-15 19:59:41 +0100

    feat(ui-tabs): change the color of unselected tabs to the same as selected ones
    This will keep the colors of normal and secondary tabs consistent
    TEST PLAN:
    check changes in docs app; run tests
    
    BREAKING CHANGE:
    SecondarySelectedColor style was removed, now just secondaryColor determines the color of the
    secondary tab.
    
    VISUAL CHANGE: secondaryColor changed to textDarkest, in the Canvas theme to
    ic-brand-font-color-dark

--- packages/ui-tabs/src/Tabs/Tab/__tests__/theme.test.js
+++ packages/ui-tabs/src/Tabs/Tab/__tests__/theme.test.js
@@ -36,7 +36,7 @@ describe('Tab.theme', async () => {
       it('should ensure text and tab background meet 3:1 contrast', async () => {
         expect(
           contrast(
-            variables.secondarySelectedColor,
+            variables.secondaryColor,
             variables.secondarySelectedBackground
           )
         ).to.be.above(3)
@@ -51,7 +51,7 @@ describe('Tab.theme', async () => {
       it('should ensure text and tab background meet 4.5:1 contrast', async () => {
         expect(
           contrast(
-            variables.secondarySelectedColor,
+            variables.secondaryColor,
             variables.secondarySelectedBackground
           )
         ).to.be.above(4.5)
