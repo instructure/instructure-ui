5893cdbea9f5d327229f0c52bad2beb46c4cbf1e
Eszter Szabo <eszter.szabo@instructure.com>
2021-02-18 14:39:30 +0100

    fix(ui-date-input): use fix 'ltr' as dir prop in DateInput storybook example

--- packages/ui-date-input/src/DateInput/__examples__/DateInput.examples.js
+++ packages/ui-date-input/src/DateInput/__examples__/DateInput.examples.js
@@ -72,7 +72,7 @@ export default {
   getExampleProps: (props) => {
     return props.isShowingCalendar
       ? {
-          dir: props.dir,
+          dir: 'ltr',
           as: 'div',
           width: '100%',
           height: '40rem',
