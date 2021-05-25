af1ebcd25042aca9bd0f1b93246a0feda9534555
Viktor Ohad <viktor.ohad@instructure.com>
2021-05-25 13:13:25 +0200

    chore(ui-docs-client): update selector design

--- packages/ui-docs-client/src/App/index.js
+++ packages/ui-docs-client/src/App/index.js
@@ -22,39 +22,39 @@
  * SOFTWARE.
  */
 
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react"
+import PropTypes from "prop-types"
 
-import GithubCorner from 'react-github-corner'
+import GithubCorner from "react-github-corner"
 
-import { themeable } from '@instructure/ui-themeable'
-import { DrawerLayout } from '@instructure/ui-drawer-layout'
-import { View } from '@instructure/ui-view'
-import { ScreenReaderContent } from '@instructure/ui-a11y-content'
-import { Mask } from '@instructure/ui-overlays'
-import { Heading } from '@instructure/ui-heading'
-import { Pill } from '@instructure/ui-pill'
-import { IconButton } from '@instructure/ui-buttons'
+import { themeable } from "@instructure/ui-themeable"
+import { DrawerLayout } from "@instructure/ui-drawer-layout"
+import { View } from "@instructure/ui-view"
+import { ScreenReaderContent } from "@instructure/ui-a11y-content"
+import { Mask } from "@instructure/ui-overlays"
+import { Heading } from "@instructure/ui-heading"
+import { Pill } from "@instructure/ui-pill"
+import { IconButton } from "@instructure/ui-buttons"
 
 import {
   IconGithubSolid,
   IconHamburgerSolid,
   IconHeartSolid,
-  IconXSolid
-} from '@instructure/ui-icons'
+  IconXSolid,
+} from "@instructure/ui-icons"
 
-import { Document } from '../Document'
-import { Header } from '../Header'
-import { Nav } from '../Nav'
-import { Theme } from '../Theme'
-import { Select } from '../Select'
-import { Section } from '../Section'
-import { Icons } from '../Icons'
-import { compileMarkdown } from '../compileMarkdown'
-import { LibraryPropType } from '../propTypes'
+import { Document } from "../Document"
+import { Header } from "../Header"
+import { Nav } from "../Nav"
+import { Theme } from "../Theme"
+import { Select } from "../Select"
+import { Section } from "../Section"
+import { Icons } from "../Icons"
+import { compileMarkdown } from "../compileMarkdown"
+import { LibraryPropType } from "../propTypes"
 
-import styles from './styles.css'
-import theme from './theme'
+import styles from "./styles.css"
+import theme from "./theme"
 
 @themeable(theme, styles)
 class App extends Component {
@@ -65,7 +65,7 @@ class App extends Component {
     themes: PropTypes.object,
     icons: PropTypes.object,
     descriptions: PropTypes.object,
-    library: LibraryPropType.isRequired
+    library: LibraryPropType.isRequired,
   }
 
   static defaultProps = {
@@ -73,75 +73,95 @@ class App extends Component {
     themes: {},
     parents: {},
     sections: {},
-    descriptions: {}
+    descriptions: {},
   }
 
   static childContextTypes = {
     library: LibraryPropType,
     themes: PropTypes.object,
-    themeKey: PropTypes.string
+    themeKey: PropTypes.string,
   }
 
-  constructor (props) {
+  constructor(props) {
     super()
 
     this.state = {
       showMenu: false,
       trayOverlay: false,
-      themeKey: Object.keys(props.themes)[0]
+      themeKey: Object.keys(props.themes)[0],
+      versionsData: {
+        latestVersion: 'v8',
+        previousVersions: ['v7', 'v6']
+      }
     }
 
     this._content = null
     this._menuTrigger = null
+
+    this.fetchVersionData()
   }
 
-  getChildContext () {
+  fetchVersionData = async () => {
+    const isLocalHost = window.location.hostname === 'localhost'
+
+    if (!isLocalHost) {
+      // eslint-disable-next-line compat/compat
+      const result = await fetch(`${window.location.origin}/versions.json`)
+      const versionsData = await result.json()
+
+      return this.setState({ versionsData })
+    }
+  }
+  getChildContext() {
     return {
       library: this.props.library,
       themeKey: this.state.themeKey,
-      themes: this.props.themes
+      themes: this.props.themes,
     }
   }
 
-  trackPage (page) {
+  trackPage(page) {
     let title = this._defaultDocumentTitle
-    if (page !== 'index') {
+    if (page !== "index") {
       title = `${page} - ${this._defaultDocumentTitle}`
     }
 
     document.title = title
 
     if (window.ga) {
-      window.ga('set', 'page', page)
-      window.ga('set', 'title', title)
-      window.ga('send', 'pageview')
+      window.ga("set", "page", page)
+      window.ga("set", "title", title)
+      window.ga("send", "pageview")
     }
   }
 
   updateKey = () => {
     const { hash } = window.location
 
-    const path = hash && hash.split('/')
+    const path = hash && hash.split("/")
 
     if (path) {
-      const [ page, id ] = path.map(entry => decodeURI(entry.replace('#', '')))
+      const [page, id] = path.map((entry) => decodeURI(entry.replace("#", "")))
 
-      this.setState({
-        key: page || 'index'
-      }, () => {
-        this.trackPage(page || 'index')
-        if (id) {
-          // After setting state, if we have an id and it corresponds to an element
-          // that exists, scroll it into view
-          const linkedSection = document.getElementById(id)
-          linkedSection && linkedSection.scrollIntoView()
-        } else if (this._content) {
-          // If we don't have an id, scroll the content back to the top
-          this._content.scrollTop = 0
+      this.setState(
+        {
+          key: page || "index",
+        },
+        () => {
+          this.trackPage(page || "index")
+          if (id) {
+            // After setting state, if we have an id and it corresponds to an element
+            // that exists, scroll it into view
+            const linkedSection = document.getElementById(id)
+            linkedSection && linkedSection.scrollIntoView()
+          } else if (this._content) {
+            // If we don't have an id, scroll the content back to the top
+            this._content.scrollTop = 0
+          }
         }
-      })
+      )
     } else {
-      this.trackPage('index')
+      this.trackPage("index")
     }
   }
 
@@ -163,7 +183,7 @@ class App extends Component {
 
   handleThemeChange = (event, option) => {
     this.setState({
-      themeKey: option.value
+      themeKey: option.value,
     })
   }
 
@@ -172,7 +192,7 @@ class App extends Component {
   }
 
   handleTrayDismiss = (e) => {
-    this.setState({showMenu: false})
+    this.setState({ showMenu: false })
   }
 
   handleTrayExited = (e) => {
@@ -181,21 +201,21 @@ class App extends Component {
     this._menuTrigger && this._menuTrigger.focus()
   }
 
-  componentDidMount () {
+  componentDidMount() {
     this._defaultDocumentTitle = document.title
     this.updateKey()
 
-    window.addEventListener('hashchange', this.updateKey, false)
+    window.addEventListener("hashchange", this.updateKey, false)
   }
 
-  componentWillUnmount () {
-    window.removeEventListener('hashchange', this.updateKey, false)
+  componentWillUnmount() {
+    window.removeEventListener("hashchange", this.updateKey, false)
   }
 
-  renderThemeSelect () {
+  renderThemeSelect() {
     const themeKeys = Object.keys(this.props.themes)
     return themeKeys.length > 1 ? (
-      <div className={styles.docsSectionHeader}>
+      <div className={styles.docsSection}>
         <div className={styles.themeSelect}>
           <Select
             name="theme"
@@ -203,7 +223,7 @@ class App extends Component {
             onChange={this.handleThemeChange}
             value={this.state.themeKey}
           >
-            {themeKeys.map(themeKey => {
+            {themeKeys.map((themeKey) => {
               return (
                 <option key={themeKey} value={themeKey}>
                   {themeKey}
@@ -216,15 +236,11 @@ class App extends Component {
     ) : null
   }
 
-  renderTheme (themeKey) {
+  renderTheme(themeKey) {
     const theme = this.props.themes[themeKey]
     return (
       <Section id={themeKey}>
-        <Heading
-          level="h1"
-          as="h2"
-          margin="0 0 medium 0"
-        >
+        <Heading level="h1" as="h2" margin="0 0 medium 0">
           {themeKey}
         </Heading>
         <Theme
@@ -237,16 +253,12 @@ class App extends Component {
     )
   }
 
-  renderIcons (key) {
+  renderIcons(key) {
     const { icons } = this.props
 
     return (
       <Section id={key}>
-        <Heading
-          level="h1"
-          as="h2"
-          margin="0 0 medium 0"
-        >
+        <Heading level="h1" as="h2" margin="0 0 medium 0">
           Iconography
         </Heading>
         <Icons
@@ -258,26 +270,32 @@ class App extends Component {
     )
   }
 
-  renderDocument (doc) {
+  renderDocument(doc) {
     const { descriptions, docs, parents } = this.props
     let children = []
 
     if (parents[doc.id]) {
-      children = parents[doc.id].children.map(childId => docs[childId])
+      children = parents[doc.id].children.map((childId) => docs[childId])
     }
 
     const description = descriptions[doc.id]
-    const heading = (doc.extension !== '.md') ? doc.title : ''
+    const heading = doc.extension !== ".md" ? doc.title : ""
 
     return (
       <div>
-        { this.renderThemeSelect() }
-        { doc.experimental && <div><Pill color="info" margin="small 0">Experimental</Pill></div>}
+        {this.renderThemeSelect()}
+        {doc.experimental && (
+          <div>
+            <Pill color="info" margin="small 0">
+              Experimental
+            </Pill>
+          </div>
+        )}
         <Section id={doc.id} heading={heading}>
           <Document
             doc={{
               ...doc,
-              children
+              children,
             }}
             description={description || doc.description}
             themeKey={this.state.themeKey}
@@ -287,48 +305,48 @@ class App extends Component {
     )
   }
 
-  renderIndex () {
+  renderIndex() {
     const { docs, library } = this.props
 
     return docs[library.name] ? (
       <Section id={library.name}>
-        {compileMarkdown(docs[library.name].description, { title: library.name })}
+        {compileMarkdown(docs[library.name].description, {
+          title: library.name,
+        })}
       </Section>
     ) : null
   }
 
-  renderChangeLog () {
+  renderChangeLog() {
     const { docs } = this.props
     return docs.CHANGELOG ? (
       <Section id="CHANGELOG">
-        {compileMarkdown(docs.CHANGELOG.description, { title: 'CHANGELOG' })}
+        {compileMarkdown(docs.CHANGELOG.description, { title: "CHANGELOG" })}
       </Section>
     ) : null
   }
 
-  renderError () {
+  renderError() {
     return (
       <Section id="error">
-        <Heading
-          level="h1"
-          as="h2"
-        >
+        <Heading level="h1" as="h2">
           Document not found
         </Heading>
       </Section>
     )
   }
 
-  renderContent (key) {
+  renderContent(key) {
     const doc = this.props.docs[key]
     const theme = this.props.themes[key]
     const icon = this.props.icons.formats[key]
 
-    if (!key || key === 'index') {
+    if (!key || key === "index") {
       return this.renderIndex()
-    } if (key === 'CHANGELOG') {
+    }
+    if (key === "CHANGELOG") {
       return this.renderChangeLog()
-    } else if (key === 'iconography' || icon) {
+    } else if (key === "iconography" || icon) {
       return this.renderIcons(key)
     } else if (theme) {
       return this.renderTheme(key)
@@ -339,41 +357,42 @@ class App extends Component {
     }
   }
 
-  renderFooter () {
-    const {
-      author,
-      repository
-    } = this.props.library
+  renderFooter() {
+    const { author, repository } = this.props.library
 
     return author || repository ? (
       <div className={styles.footer}>
-        { author && (
+        {author && (
           <span>
-            Made with &nbsp;
+            Made with &nbsp
             <IconHeartSolid className={styles.footerIcon} />
-            &nbsp; by {author}. &nbsp;
+            &nbsp by {author}. &nbsp
           </span>
-        ) }
-        { repository && (
-          <a href={repository} rel="noopener noreferrer" className={styles.githubLink} target="_blank">
+        )}
+        {repository && (
+          <a
+            href={repository}
+            rel="noopener noreferrer"
+            className={styles.githubLink}
+            target="_blank"
+          >
             <IconGithubSolid className={styles.footerIcon} />
             <ScreenReaderContent>Contribute on Github</ScreenReaderContent>
           </a>
-        ) }
+        )}
       </div>
     ) : null
   }
 
-  render () {
-    const {
-      name,
-      version,
-      repository
-    } = this.props.library
+  render() {
+    const { name, version, repository } = this.props.library
+    const { versionsData } = this.state
 
     return (
       <div className={styles.root}>
-        { this.state.trayOverlay && this.state.showMenu && <Mask onClick={this.handleMenuToggle} /> }
+        {this.state.trayOverlay && this.state.showMenu && (
+          <Mask onClick={this.handleMenuToggle} />
+        )}
         <DrawerLayout onOverlayTrayChange={this.handleOverlayTrayChange}>
           <DrawerLayout.Tray
             label="Navigation"
@@ -404,7 +423,7 @@ class App extends Component {
                   margin="small small none none"
                 />
               </View>
-              <Header name={name} version={version} />
+              <Header name={name} version={version} versionsData={versionsData} />
               <Nav
                 selected={this.state.key}
                 sections={this.props.sections}
@@ -445,7 +464,7 @@ class App extends Component {
             </View>
           </DrawerLayout.Content>
         </DrawerLayout>
-        { repository && <GithubCorner href={repository} /> }
+        {repository && <GithubCorner href={repository} />}
       </div>
     )
   }
