7b0c6f440fc682993213cebc8f7c5f1b4f15d9bc
Bence Toppanto <bence.toppanto@instructure.com>
2021-07-13 14:41:38 +0200

    feat(ui-time-select): replace moment.js with dayjs. We only support locales, supported by canvas. (https://community.canvaslms.com/t5/Canvas-Basics-Guide/What-languages-does-Canvas-support/ta-p/19)

---
+++ packages/ui-time-select/src/TimeSelect/locales.ts
@@ -0,0 +1,59 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 - present Instructure, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import 'dayjs/locale/ar'
+import 'dayjs/locale/hy-am'
+import 'dayjs/locale/ca'
+import 'dayjs/locale/da'
+import 'dayjs/locale/nl'
+import 'dayjs/locale/en-au'
+import 'dayjs/locale/en-ca'
+import 'dayjs/locale/en-gb'
+import 'dayjs/locale/fi'
+import 'dayjs/locale/fr'
+import 'dayjs/locale/fr-ca'
+import 'dayjs/locale/de'
+import 'dayjs/locale/el'
+import 'dayjs/locale/ht'
+import 'dayjs/locale/he'
+import 'dayjs/locale/hu'
+import 'dayjs/locale/is'
+import 'dayjs/locale/it'
+import 'dayjs/locale/ja'
+import 'dayjs/locale/ko'
+import 'dayjs/locale/mi'
+import 'dayjs/locale/nb'
+import 'dayjs/locale/fa'
+import 'dayjs/locale/pl'
+import 'dayjs/locale/pt'
+import 'dayjs/locale/pt-br'
+import 'dayjs/locale/ru'
+import 'dayjs/locale/zh'
+import 'dayjs/locale/sl'
+import 'dayjs/locale/es'
+import 'dayjs/locale/sv'
+import 'dayjs/locale/zh-cn'
+import 'dayjs/locale/tr'
+import 'dayjs/locale/uk'
+import 'dayjs/locale/cy'
