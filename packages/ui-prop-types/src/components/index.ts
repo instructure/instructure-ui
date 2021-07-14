cb0550992f0cfbc1f599f9ca6e3305ef78a73931
Viktor Ohad <viktor.ohad@instructure.com>
2021-07-14 18:34:26 +0200

    chore(emotion,ui-prop-types): extend `ComponentThemeMap` with `Billboard, Breadcrumb, Buttons`

--- packages/ui-prop-types/src/components/index.ts
+++ packages/ui-prop-types/src/components/index.ts
@@ -21,7 +21,15 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-import { BaseTheme, Border, Typography } from '@instructure/ui-theme-tokens'
+import {
+  BaseTheme,
+  Border,
+  Forms,
+  Shadows,
+  Spacing,
+  Stacking,
+  Typography
+} from '@instructure/ui-theme-tokens'
 
 type AvatarThemeVariables = {
   color: BaseTheme['colors']['textBrand']
@@ -33,13 +41,180 @@ type AvatarThemeVariables = {
   fontWeight: Typography['fontWeightBold']
 }
 
+type AlertThemeVariables = {
+  background: BaseTheme['colors']['backgroundLightest']
+  color: BaseTheme['colors']['textDarkest']
+  marginTop: Spacing['small']
+  borderRadius: Border['radiusMedium']
+  borderWidth: Border['widthMedium']
+  borderStyle: Border['style']
+  contentPadding: `${Spacing['small']} ${Spacing['medium']} `
+  contentFontSize: Typography['fontSizeMedium']
+  contentFontFamily: Typography['fontFamily']
+  contentFontWeight: Typography['fontWeightNormal']
+  contentLineHeight: Typography['lineHeightCondensed']
+  closeButtonMarginTop: Spacing['xSmall']
+  closeButtonMarginRight: Spacing['xxSmall']
+  iconColor: BaseTheme['colors']['textLightest']
+  successBorderColor: BaseTheme['colors']['borderSuccess']
+  successIconBackground: BaseTheme['colors']['backgroundSuccess']
+  infoBorderColor: BaseTheme['colors']['borderInfo']
+  infoIconBackground: BaseTheme['colors']['backgroundInfo']
+  warningBorderColor: BaseTheme['colors']['borderWarning']
+  warningIconBackground: BaseTheme['colors']['backgroundWarning']
+  dangerBorderColor: BaseTheme['colors']['borderDanger']
+  dangerIconBackground: BaseTheme['colors']['backgroundDanger']
+  boxShadow: Shadows['depth2']
+}
+
+type BadgeThemeVariables = {
+  fontFamily: Typography['fontFamily']
+  fontWeight: Typography['fontWeightNormal']
+  color: BaseTheme['colors']['textLightest']
+  fontSize: Typography['fontSizeXSmall']
+  colorDanger: BaseTheme['colors']['textDanger']
+  colorSuccess: BaseTheme['colors']['textSuccess']
+  colorPrimary: BaseTheme['colors']['textBrand']
+  notificationZIndex: Stacking['above']
+  sizeNotification: Spacing['small']
+  padding: Spacing['xxSmall']
+  pulseBorderThickness: Border['widthMedium']
+}
+
+type BillboardThemeVariables = {
+  fontFamily: Typography['fontFamily']
+  paddingSmall: Spacing['small']
+  paddingMedium: Spacing['medium']
+  paddingLarge: Spacing['medium']
+  iconColor: BaseTheme['colors']['textDark']
+  mediumMargin: Spacing['small']
+  largeMargin: Spacing['medium']
+  iconHoverColor: BaseTheme['colors']['textLink']
+  backgroundColor: BaseTheme['colors']['backgroundLightest']
+  iconHoverColorInverse: BaseTheme['colors']['textLightest']
+  buttonBorderWidth: Border['widthMedium']
+  buttonBorderRadius: Border['radiusLarge']
+  messageColor: BaseTheme['colors']['textDark']
+  messageColorClickable: BaseTheme['colors']['textLink']
+  messageColorInverse: BaseTheme['colors']['textLight']
+  messageFontSizeSmall: Typography['fontSizeSmall']
+  messageFontSizeMedium: Typography['fontSizeMedium']
+  messageFontSizeLarge: Typography['fontSizeLarge']
+  clickableActiveBg: BaseTheme['colors']['backgroundBrand']
+  clickableActiveText: BaseTheme['colors']['textLightest']
+  buttonBorderStyle: Border['style']
+}
+
+type BreadcrumbThemeVariables = {
+  fontFamily: Typography['fontFamily']
+  separatorColor: BaseTheme['colors']['borderDark']
+  smallFontSize: Typography['fontSizeSmall']
+  mediumFontSize: Typography['fontSizeMedium']
+  largeFontSize: Typography['fontSizeLarge']
+}
+
+type BaseButtonThemeVariables = {
+  fontFamily: Typography['fontFamily']
+  fontWeight: Typography['fontWeightNormal']
+  borderRadius: Border['radiusMedium']
+  borderStyle: Border['style']
+  borderWidth: Border['widthSmall']
+  smallHeight: Forms['inputHeightSmall']
+  smallFontSize: Typography['fontSizeSmall']
+  smallPaddingHorizontal: Spacing['xSmall']
+  mediumHeight: Forms['inputHeightMedium']
+  mediumFontSize: Typography['fontSizeMedium']
+  mediumPaddingHorizontal: Spacing['small']
+  largeHeight: Forms['inputHeightLarge']
+  largeFontSize: Typography['fontSizeLarge']
+  largePaddingHorizontal: Spacing['medium']
+  lineHeight: Typography['lineHeightFit']
+  iconTextGap: Spacing['xSmall']
+  iconTextGapCondensed: Spacing['xxSmall']
+
+  primaryColor: string
+  primaryBorderColor: string
+  primaryBackground: string
+  primaryHoverBackground: string
+  primaryActiveBackground: string
+  primaryActiveBoxShadow: string
+  primaryGhostColor: string
+  primaryGhostBorderColor: string
+  primaryGhostBackground: string
+  primaryGhostHoverBackground: string
+  primaryGhostActiveBackground: string
+  primaryGhostActiveBoxShadow: string
+
+  secondaryBorderColor: string
+  secondaryBackground: string
+  secondaryHoverBackground: string
+  secondaryActiveBackground: string
+  secondaryActiveBoxShadow: string
+  secondaryGhostColor: string
+  secondaryGhostBorderColor: string
+  secondaryGhostBackground: string
+  secondaryGhostHoverBackground: string
+  secondaryGhostActiveBackground: string
+  secondaryGhostActiveBoxShadow: string
+
+  successBorderColor: string
+  successBackground: string
+  successHoverBackground: string
+  successActiveBackground: string
+  successActiveBoxShadow: string
+  successGhostColor: string
+  successGhostBorderColor: string
+  successGhostBackground: string
+  successGhostHoverBackground: string
+  successGhostActiveBackground: string
+  successGhostActiveBoxShadow: string
+
+  dangerBorderColor: string
+  dangerBackground: string
+  dangerHoverBackground: string
+  dangerActiveBackground: string
+  dangerActiveBoxShadow: string
+  dangerGhostColor: string
+  dangerGhostBorderColor: string
+  dangerGhostBackground: string
+  dangerGhostHoverBackground: string
+  dangerGhostActiveBackground: string
+  dangerGhostActiveBoxShadow: string
+
+  primaryInverseBorderColor: string
+  primaryInverseBackground: string
+  primaryInverseHoverBackground: string
+  primaryInverseActiveBackground: string
+  primaryInverseActiveBoxShadow: string
+  primaryInverseGhostColor: string
+  primaryInverseGhostBorderColor: string
+  primaryInverseGhostBackground: string
+  primaryInverseGhostHoverBackground: string
+  primaryInverseGhostActiveBackground: string
+  primaryInverseGhostActiveBoxShadow: string
+}
+
+type CloseButtonThemeVariables = {
+  offsetMedium: Spacing['medium']
+  offsetSmall: Spacing['small']
+  offsetXSmall: Spacing['xSmall']
+  zIndex: Stacking['above']
+}
 type ThemeVariables = {
-  Avatar: Partial<AvatarThemeVariables>
+  Avatar: AvatarThemeVariables
+  Alert: AlertThemeVariables
+  Badge: BadgeThemeVariables
+  Billboard: BillboardThemeVariables
+  Breadcrumb: BreadcrumbThemeVariables
+  BaseButton: BaseButtonThemeVariables
+  Button: BaseButtonThemeVariables
+  CloseButton: CloseButtonThemeVariables
+  CondensedButton: BaseButtonThemeVariables
+  IconButton: BaseButtonThemeVariables
 }
 type ComponentThemeMap = ComponentTheme<ThemeVariables>
 
 type ComponentTheme<Type> = {
   [Key in keyof Type]: Type[Key]
 }
-
 export type { ThemeVariables, ComponentThemeMap, AvatarThemeVariables }
