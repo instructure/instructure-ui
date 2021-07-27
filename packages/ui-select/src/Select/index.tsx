f21ae20d8d4fd4069e0a5ca8ffc8574c11f7c329
Matyas Forian-Szabo <matyas.szabo@instructure.com>
2021-07-27 14:04:49 +0200

    docs(ui-select,ui-selectable): documentation fixes

--- packages/ui-select/src/Select/index.tsx
+++ packages/ui-select/src/Select/index.tsx
@@ -105,8 +105,7 @@ type Props = {
 category: components
 tags: autocomplete, typeahead, combobox, dropdown, search, form
 ---
-@tsProps
- **/
+**/
 @withStyle(generateStyle, generateComponentTheme)
 @testable()
 class Select extends Component<Props> {
