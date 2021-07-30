c90f4887d478d0b80b8eb7922293240bca7c6eb0
Viktor Ohad <viktor.ohad@instructure.com>
2021-07-30 13:16:36 +0200

    test(ui-test-utils): fix generateComponentExample test

--- packages/ui-test-utils/src/__tests__/generateComponentExamples.test.ts
+++ packages/ui-test-utils/src/__tests__/generateComponentExamples.test.ts
@@ -24,7 +24,7 @@
 
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { expect } from '../'
+import { expect } from '..'
 
 import { generateComponentExamples } from '../utils/generateComponentExamples'
 
