645ddec16d28a8c25dbc57ea97137df3748ca7e7
Viktor Ohad <viktor.ohad@instructure.com>
2021-09-03 12:04:25 +0200

    chore(ui-view): add allowedProps
    Closes: INSTUI-3230

--- packages/ui-view/src/ContextView/props.ts
+++ packages/ui-view/src/ContextView/props.ts
@@ -21,12 +21,21 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+import React from 'react'
+import PropTypes from 'prop-types'
 
-import type { CSSObject, Shadow, Spacing, Stacking } from '@instructure/emotion'
-import type { AsElementType } from '@instructure/shared-types'
+import { ThemeablePropTypes } from '@instructure/emotion'
+import { PositionPropTypes } from '@instructure/ui-position'
+import type {
+  Shadow,
+  Spacing,
+  Stacking,
+  WithStyleProps
+} from '@instructure/emotion'
+import type { AsElementType, PropValidators } from '@instructure/shared-types'
 import type { PlacementPropValues } from '@instructure/ui-position'
 
-export type ContextViewProps = {
+export type ContextViewOwnProps = React.PropsWithChildren<{
   as?: AsElementType
   elementRef?: (...args: any[]) => any
   height?: string | number
@@ -38,11 +47,107 @@ export type ContextViewProps = {
   textAlign?: 'start' | 'center' | 'end'
   background?: 'default' | 'inverse'
   debug?: boolean
-  makeStyles?: (...args: any[]) => any
-  styles?: CSSObject
-  margin: Spacing
-  padding: Spacing
-  shadow: Shadow
-  stacking: Stacking
-  placement: PlacementPropValues
+  margin?: Spacing
+  padding?: Spacing
+  shadow?: Shadow
+  stacking?: Stacking
+  placement?: PlacementPropValues
+}>
+
+type PropKeys = keyof ContextViewOwnProps
+
+type AllowedPropKeys = Readonly<Array<PropKeys>>
+
+type ContextViewProps = ContextViewOwnProps & WithStyleProps
+
+const propTypes: PropValidators<PropKeys> = {
+  /**
+   * The element to render as the component root
+   */
+  as: PropTypes.elementType,
+
+  /**
+   * provides a reference to the underlying html root element
+   */
+  elementRef: PropTypes.func,
+
+  /**
+   * Valid values are `0`, `none`, `auto`, `xxx-small`, `xx-small`, `x-small`,
+   * `small`, `medium`, `large`, `x-large`, `xx-large`. Apply these values via
+   * familiar CSS-like shorthand. For example: `margin="small auto large"`.
+   */
+  margin: ThemeablePropTypes.spacing,
+  /**
+   * Valid values are `0`, `none`, `xxx-small`, `xx-small`, `x-small`,
+   * `small`, `medium`, `large`, `x-large`, `xx-large`. Apply these values via
+   * familiar CSS-like shorthand. For example: `padding="small x-large large"`.
+   */
+  padding: ThemeablePropTypes.spacing,
+
+  height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  maxHeight: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  maxWidth: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  minHeight: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  minWidth: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+
+  /**
+   * The children to render inside the `<ContextView />`
+   */
+  children: PropTypes.node,
+
+  /**
+   * Designates the text alignment within the `<ContextView />`
+   */
+  textAlign: PropTypes.oneOf(['start', 'center', 'end']),
+
+  /**
+   * Controls the shadow depth for the `<ContextView />`
+   */
+  shadow: ThemeablePropTypes.shadow,
+
+  /**
+   * Controls the z-index depth for the `<ContextView />`
+   */
+  stacking: ThemeablePropTypes.stacking,
+
+  /**
+   * Designates the background style of the `<ContextView />`
+   */
+  background: PropTypes.oneOf(['default', 'inverse']),
+
+  /**
+   * Specifies how the arrow for `<ContextView />` will be rendered.
+   * Ex. `placement="top"` will render with an arrow pointing down.
+   */
+  placement: PositionPropTypes.placement,
+
+  /**
+   * Activate an outline around the component to make building your
+   * layout easier
+   */
+  debug: PropTypes.bool
 }
+
+const allowedProps: AllowedPropKeys = [
+  'as',
+  'elementRef',
+  'margin',
+  'padding',
+  'height',
+  'width',
+  'maxHeight',
+  'maxWidth',
+  'minHeight',
+  'minWidth',
+  'children',
+  'textAlign',
+  'shadow',
+  'stacking',
+  'background',
+  'placement',
+  'debug'
+]
+
+export type { ContextViewProps }
+export { propTypes, allowedProps }
