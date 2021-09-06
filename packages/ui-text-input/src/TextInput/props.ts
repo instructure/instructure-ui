dde1a48894414b0436e7be68911d3dda92e079a5
Viktor Ohad <viktor.ohad@instructure.com>
2021-09-06 14:32:58 +0200

    chore(ui-text-input): add `allowedProps`

--- packages/ui-text-input/src/TextInput/props.ts
+++ packages/ui-text-input/src/TextInput/props.ts
@@ -23,10 +23,16 @@
  */
 
 import React from 'react'
+import PropTypes from 'prop-types'
+
+import { FormPropTypes } from '@instructure/ui-form-field'
+import { controllable } from '@instructure/ui-prop-types'
 import type { InteractionType } from '@instructure/ui-react-utils'
 import type { FormMessage } from '@instructure/ui-form-field'
+import type { PropValidators } from '@instructure/shared-types'
+import type { WithStyleProps } from '@instructure/emotion'
 
-export type TextInputProps = {
+export type TextInputOwnProps = {
   renderLabel?: React.ReactNode | ((...args: any[]) => any)
   type?: 'text' | 'email' | 'url' | 'tel' | 'search' | 'password'
   id?: string
@@ -49,16 +55,151 @@ export type TextInputProps = {
   onChange?: (...args: any[]) => any
   onBlur?: (...args: any[]) => any
   onFocus?: (...args: any[]) => any
-  makeStyles?: (...args: any[]) => any
-  styles?: any
 }
 
-export type TextInputState = {
+type PropKeys = keyof TextInputOwnProps
+
+type AllowedPropKeys = Readonly<Array<PropKeys>>
+
+type TextInputProps = TextInputOwnProps & WithStyleProps
+
+const propTypes: PropValidators<PropKeys> = {
+  /**
+   * The form field label.
+   */
+  renderLabel: PropTypes.oneOfType([PropTypes.node, PropTypes.func]),
+  /**
+   * Determines the underlying native HTML `<input>` element's `type`.
+   * For more see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/url
+   */
+  type: PropTypes.oneOf(['text', 'email', 'url', 'tel', 'search', 'password']),
+  /**
+   * The id of the text input. One is generated if not supplied.
+   */
+  id: PropTypes.string,
+  /**
+   * the selected value (must be accompanied by an `onChange` prop)
+   */
+  value: controllable(PropTypes.string),
+  /**
+   * value to set on initial render
+   */
+  defaultValue: PropTypes.string,
+  /**
+   * Specifies if interaction with the input is enabled, disabled, or readonly.
+   * When "disabled", the input changes visibly to indicate that it cannot
+   * receive user interactions. When "readonly" the input still cannot receive
+   * user interactions but it keeps the same styles as if it were enabled.
+   */
+  interaction: PropTypes.oneOf(['enabled', 'disabled', 'readonly']),
+  /**
+   * object with shape: `{
+   * text: PropTypes.string,
+   * type: PropTypes.oneOf(['error', 'hint', 'success', 'screenreader-only'])
+   *   }`
+   */
+  messages: PropTypes.arrayOf(FormPropTypes.message),
+  /**
+   * The size of the text input.
+   */
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  /**
+   * The text alignment of the input.
+   */
+  textAlign: PropTypes.oneOf(['start', 'center']),
+  /**
+   * The width of the input.
+   */
+  width: PropTypes.string,
+  /**
+   * The width of the input, in characters, if a width is not explicitly
+   * provided via the `width` prop. Only applicable if `isInline={true}`.
+   */
+  htmlSize: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /**
+   * The display of the root element.
+   */
+  display: PropTypes.oneOf(['inline-block', 'block']),
+  /**
+   * Prevents the default behavior of wrapping the input and rendered content
+   * when available space is exceeded.
+   */
+  shouldNotWrap: PropTypes.bool,
+  /**
+   * Html placeholder text to display when the input has no value. This should be hint text, not a label
+   * replacement.
+   */
+  placeholder: PropTypes.string,
+  /**
+   * Whether or not the text input is required.
+   */
+  isRequired: PropTypes.bool,
+  /**
+   * a function that provides a reference to the actual input element
+   */
+  inputRef: PropTypes.func,
+  /**
+   * a function that provides a reference a parent of the input element
+   */
+  inputContainerRef: PropTypes.func,
+  /**
+   * Content to display before the input text, such as an icon
+   */
+  renderBeforeInput: PropTypes.oneOfType([PropTypes.node, PropTypes.func]),
+  /**
+   * Content to display after the input text, such as an icon
+   */
+  renderAfterInput: PropTypes.oneOfType([PropTypes.node, PropTypes.func]),
+  /**
+   * Callback executed when the input fires a change event.
+   * @param {Object} event - the event object
+   * @param {Object} value - the string value of the input
+   */
+  onChange: PropTypes.func,
+  /**
+   * Callback fired when input loses focus.
+   */
+  onBlur: PropTypes.func,
+  /**
+   * Callback fired when input receives focus.
+   */
+  onFocus: PropTypes.func
+}
+
+const allowedProps: AllowedPropKeys = [
+  'renderLabel',
+  'type',
+  'id',
+  'value',
+  'defaultValue',
+  'interaction',
+  'messages',
+  'size',
+  'textAlign',
+  'width',
+  'htmlSize',
+  'display',
+  'shouldNotWrap',
+  'placeholder',
+  'isRequired',
+  'inputRef',
+  'inputContainerRef',
+  'renderBeforeInput',
+  'renderAfterInput',
+  'onChange',
+  'onBlur',
+  'onFocus'
+]
+
+type TextInputState = {
   hasFocus: boolean
 }
 
-export type TextInputStyleProps = {
+type TextInputStyleProps = {
   disabled: boolean
   invalid: boolean
   focused: TextInputState['hasFocus']
 }
+
+export type { TextInputProps, TextInputState, TextInputStyleProps }
+export { propTypes, allowedProps }
