8d0c1f4493e184102bbe848e57c3cd41c75ef273
Viktor Ohad <viktor.ohad@instructure.com>
2021-09-07 09:49:31 +0200

    chore(ui-spinner): add `allowedProps`

--- packages/ui-spinner/src/Spinner/props.ts
+++ packages/ui-spinner/src/Spinner/props.ts
@@ -21,13 +21,13 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+import PropTypes from 'prop-types'
+import { ThemeablePropTypes } from '@instructure/emotion'
 
-import type { Spacing } from '@instructure/emotion'
-import { AsElementType } from '@instructure/shared-types'
+import type { Spacing, WithStyleProps } from '@instructure/emotion'
+import type { AsElementType, PropValidators } from '@instructure/shared-types'
 
-export type SpinnerProps = {
-  makeStyles?: (...args: any[]) => any
-  styles?: any
+type SpinnerOwnProps = {
   renderTitle?: ((...args: any[]) => any) | React.ReactNode
   size?: 'x-small' | 'small' | 'medium' | 'large'
   variant?: 'default' | 'inverse'
@@ -35,3 +35,44 @@ export type SpinnerProps = {
   elementRef?: (...args: any[]) => any
   as?: AsElementType
 }
+
+type PropKeys = keyof SpinnerOwnProps
+
+type AllowedPropKeys = Readonly<Array<PropKeys>>
+
+type SpinnerProps = SpinnerOwnProps & WithStyleProps
+
+const propTypes: PropValidators<PropKeys> = {
+  /**
+   * Give the spinner a title to be read by screenreaders
+   */
+  renderTitle: PropTypes.oneOfType([PropTypes.func, PropTypes.node]),
+  /**
+   * Different-sized spinners
+   */
+  size: PropTypes.oneOf(['x-small', 'small', 'medium', 'large']),
+  /**
+   * Different color schemes for use with light or dark backgrounds
+   */
+  variant: PropTypes.oneOf(['default', 'inverse']),
+  /**
+   * Valid values are `0`, `none`, `auto`, `xxx-small`, `xx-small`, `x-small`,
+   * `small`, `medium`, `large`, `x-large`, `xx-large`. Apply these values via
+   * familiar CSS-like shorthand. For example: `margin="small auto large"`.
+   */
+  margin: ThemeablePropTypes.spacing,
+  elementRef: PropTypes.func,
+  as: PropTypes.elementType
+}
+
+const allowedProps: AllowedPropKeys = [
+  'renderTitle',
+  'size',
+  'variant',
+  'margin',
+  'elementRef',
+  'as'
+]
+
+export type { SpinnerProps }
+export { propTypes, allowedProps }
