07f3617fcd62e4597788c20651fad3d0a7baedfa
Viktor Ohad <viktor.ohad@instructure.com>
2021-09-07 13:33:28 +0200

    chore(ui-range-input): add `allowedProps`

--- packages/ui-range-input/src/RangeInput/props.ts
+++ packages/ui-range-input/src/RangeInput/props.ts
@@ -21,10 +21,15 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+import PropTypes from 'prop-types'
+import { controllable } from '@instructure/ui-prop-types'
+import { FormPropTypes } from '@instructure/ui-form-field'
 
+import { PropValidators } from '@instructure/shared-types'
 import type { FormMessage } from '@instructure/ui-form-field'
+import type { WithStyleProps } from '@instructure/emotion'
 
-export type RangeInputProps = {
+export type RangeInputOwnProps = {
   min: number
   max: number
   defaultValue?: number
@@ -41,6 +46,68 @@ export type RangeInputProps = {
   inline?: boolean
   disabled?: boolean
   readOnly?: boolean
-  makeStyles?: (...args: any[]) => any
-  styles?: any
 }
+type PropKeys = keyof RangeInputOwnProps
+
+type AllowedPropKeys = Readonly<Array<PropKeys>>
+
+type RangeInputProps = RangeInputOwnProps & WithStyleProps
+
+const propTypes: PropValidators<PropKeys> = {
+  min: PropTypes.number.isRequired,
+  max: PropTypes.number.isRequired,
+  /**
+   * value to set on initial render
+   */
+  defaultValue: PropTypes.number,
+  /**
+   * the selected value (must be accompanied by an `onChange` prop)
+   */
+  value: controllable(PropTypes.number),
+  /**
+   * when used with the `value` prop, the component will not control its own state
+   */
+  onChange: PropTypes.func,
+  messages: PropTypes.arrayOf(FormPropTypes.message),
+  /**
+   * The size of the value label
+   */
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  layout: PropTypes.oneOf(['stacked', 'inline']),
+  id: PropTypes.string,
+  label: PropTypes.node.isRequired,
+  /**
+   * whether to display the current value
+   */
+  displayValue: PropTypes.bool,
+  step: PropTypes.number,
+  /**
+   * A function to format the displayed value
+   */
+  formatValue: PropTypes.func,
+  inline: PropTypes.bool,
+  disabled: PropTypes.bool,
+  readOnly: PropTypes.bool
+}
+
+const allowedProps: AllowedPropKeys = [
+  'min',
+  'max',
+  'defaultValue',
+  'value',
+  'onChange',
+  'messages',
+  'size',
+  'layout',
+  'id',
+  'label',
+  'displayValue',
+  'step',
+  'formatValue',
+  'inline',
+  'disabled',
+  'readOnly'
+]
+
+export type { RangeInputProps }
+export { propTypes, allowedProps }
