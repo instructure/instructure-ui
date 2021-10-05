04c938d96a3d57cea6877a37b09c2eaafa59b97f
Eszter Szabo <eszter.szabo@instructure.com>
2021-10-05 17:57:24 +0200

    chore(ui-img): add  TS types to `ui-img` package
    Closes: INSTUI-3056

--- packages/ui-img/src/Img/index.tsx
+++ packages/ui-img/src/Img/index.tsx
@@ -40,6 +40,7 @@ import type { ImgProps } from './props'
 ---
 category: components
 ---
+@tsProps
 **/
 @withStyle(generateStyle, generateComponentTheme)
 @testable()
@@ -63,13 +64,11 @@ class Img extends Component<ImgProps> {
   }
 
   componentDidMount() {
-    // @ts-expect-error ts-migrate(2722) FIXME: Cannot invoke an object which is possibly 'undefin... Remove this comment to see the full error message
-    this.props.makeStyles()
+    this.props.makeStyles?.()
   }
 
   componentDidUpdate() {
-    // @ts-expect-error ts-migrate(2722) FIXME: Cannot invoke an object which is possibly 'undefin... Remove this comment to see the full error message
-    this.props.makeStyles()
+    this.props.makeStyles?.()
   }
 
   render() {
