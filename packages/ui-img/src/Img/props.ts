d099d3fafa13ef64bd0a35038f88245cfd299aa8
Eszter Szabo <eszter.szabo@instructure.com>
2021-10-05 17:57:24 +0200

    chore(ui-img): add  TS types to `ui-img` package
    Closes: INSTUI-3056

--- packages/ui-img/src/Img/props.ts
+++ packages/ui-img/src/Img/props.ts
@@ -36,7 +36,16 @@ type ImgOwnProps = {
   src: string
   alt?: string
   display?: 'inline-block' | 'block'
+  /**
+   * Valid values are `0`, `none`, `auto`, `xxx-small`, `xx-small`, `x-small`,
+   * `small`, `medium`, `large`, `x-large`, `xx-large`. Apply these values via
+   * familiar CSS-like shorthand. For example: `margin="small auto large"`.
+   */
   margin?: Spacing
+  /**
+   * Valid values for `opacity` are `0` - `10`. Valid values for `blend` are
+   * `normal` (default), `multiply`, `screen`, `overlay`, and `color-burn`.
+   */
   overlay?: {
     color: string
     opacity: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
@@ -62,16 +71,7 @@ const propTypes: PropValidators<PropKeys> = {
   src: PropTypes.string.isRequired,
   alt: PropTypes.string,
   display: PropTypes.oneOf(['inline-block', 'block']),
-  /**
-   * Valid values are `0`, `none`, `auto`, `xxx-small`, `xx-small`, `x-small`,
-   * `small`, `medium`, `large`, `x-large`, `xx-large`. Apply these values via
-   * familiar CSS-like shorthand. For example: `margin="small auto large"`.
-   */
   margin: ThemeablePropTypes.spacing,
-  /**
-   * Valid values for `opacity` are `0` - `10`. Valid values for `blend` are
-   * `normal` (default), `multiply`, `screen`, `overlay`, and `color-burn`.
-   */
   overlay: PropTypes.shape({
     color: PropTypes.string.isRequired,
     opacity: PropTypes.oneOf([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).isRequired,
