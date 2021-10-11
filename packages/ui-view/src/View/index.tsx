48146521e58bef8e0f75c299278072c715448277
Bence Toppanto <bence.toppanto@instructure.com>
2021-10-11 10:07:51 +0200

    feat(ui-view): vendor prefix all view style props
    Closes: INSTUI-3271
    
    Adding prop-based styling had no vendor prefixing.
    Added inline styles to emotion's css prop and
    added !important to every rule in it

--- packages/ui-view/src/View/index.tsx
+++ packages/ui-view/src/View/index.tsx
@@ -218,8 +218,7 @@ class View extends Component<ViewProps> {
         //@ts-expect-error TODO: `ref` prop causes: "Expression produces a union type that is too complex to represent.ts(2590)"
         {...passthroughProps(props)}
         className={className}
-        css={styles?.view}
-        style={styles?.inlineStyles}
+        css={[styles?.view, styles?.inlineStyles]}
         ref={this.handleElementRef}
       >
         {children}
