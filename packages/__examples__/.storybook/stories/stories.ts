eeea0c79e34b660ce4fb7b2bf8f8edb25b74ba9c
Matyas Forian-Szabo <matyas.szabo@instructure.com>
2021-10-12 15:31:15 +0200

    refactor: debugging Chromatic build error

--- packages/__examples__/.storybook/stories/stories.ts
+++ packages/__examples__/.storybook/stories/stories.ts
@@ -60,7 +60,9 @@ console.log(
     { exampleFilePath: string; generatedPropValues: Record<string, any> }
   ]) => {
     const requirePath = `./${exampleFilePath}`
-
+    if (numStories > 30) {
+      return
+    }
     const exampleDir = requirePath.split('/').slice(0, -2).join('/')
     try {
       const Component: ComponentType = componentsContext(
