54eb2f2563543f1d2219135ab6f6cce1c5434cb1
Matyas Forian-Szabo <matyas.szabo@instructure.com>
2021-12-17 14:22:52 +0100

    refactor(ui-axe-check): better TypeScript types

--- packages/ui-axe-check/src/runAxeCheck.ts
+++ packages/ui-axe-check/src/runAxeCheck.ts
@@ -21,14 +21,25 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+
 import axeCore from 'axe-core'
 
-// @ts-expect-error ts-migrate(7006) FIXME: Parameter 'element' implicitly has an 'any' type.
-export default async function runAxe(element, options = {}) {
-  let result = true
-  const context = {
+type AxeCheckOptions = {
+  exclude?: string | string[]
+  /**
+   * Violations to ignore/filter out. 'color-contrast' is automatically excluded
+   * because we test color contrast in theme tests.
+   */
+  ignores?: string[]
+}
+
+export default async function runAxe(
+  element: string,
+  options: AxeCheckOptions = {}
+) {
+  let result: Error | true = true
+  const context: axeCore.ContextObject = {
     include: [element],
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'exclude' does not exist on type '{}'.
     exclude: options.exclude
   }
   const config = {
@@ -36,15 +47,13 @@ export default async function runAxe(element, options = {}) {
       type: 'tag',
       values: ['wcag2a', 'wcag2aa', 'section508', 'best-practice']
     }
-  }
+  } as axeCore.RunOptions
 
   try {
-    // @ts-expect-error ts-migrate(2769) FIXME: No overload matches this call.
     const axeResult = await axeCore.run(context, config)
 
     // violations to ignore/filter out
     const ignores = [
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'ignores' does not exist on type '{}'.
       ...(options.ignores || []),
       'aria-hidden-focus', // TODO: remove this and fix the broken tests
       'color-contrast' // because we test color contrast in theme tests
@@ -70,7 +79,6 @@ export default async function runAxe(element, options = {}) {
     })
 
     if (violations.length > 0) {
-      // @ts-expect-error ts-migrate(2322) FIXME: Type 'Error' is not assignable to type 'boolean'.
       result = new Error(formatError(violations))
     }
   } catch (err: any) {
@@ -79,20 +87,19 @@ export default async function runAxe(element, options = {}) {
   return result
 }
 
-// @ts-expect-error ts-migrate(7006) FIXME: Parameter 'violations' implicitly has an 'any' typ... Remove this comment to see the full error message
-function formatError(violations) {
-  // @ts-expect-error ts-migrate(7006) FIXME: Parameter 'violation' implicitly has an 'any' type... Remove this comment to see the full error message
-  return violations.map((violation) => {
-    return [
-      `[${violation.id}] ${violation.help}`,
-      violation.nodes
-        // @ts-expect-error ts-migrate(7006) FIXME: Parameter 'node' implicitly has an 'any' type.
-        .map(function (node) {
-          return node.target.toString()
-        })
-        .join('\n'),
-      violation.description,
-      `${violation.helpUrl}\n`
-    ].join('\n')
-  })
+function formatError(violations: axeCore.Result[]) {
+  return violations
+    .map((violation) => {
+      return [
+        `[${violation.id}] ${violation.help}`,
+        violation.nodes
+          .map(function (node) {
+            return node.target.toString()
+          })
+          .join('\n'),
+        violation.description,
+        `${violation.helpUrl}\n`
+      ].join('\n')
+    })
+    .join('\n')
 }
