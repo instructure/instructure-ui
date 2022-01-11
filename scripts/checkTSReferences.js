7e026aa6bf1fb335d3dc73759deba55d841a7f13
Viktor Ohad <viktor.ohad@instructure.com>
2022-01-11 10:54:52 +0100

    chore(ui-text): fix ts reference mismatch, fix typos

--- scripts/checkTSReferences.js
+++ scripts/checkTSReferences.js
@@ -22,14 +22,14 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-var-requires */
 
 /**
  * This simple script will check if there is any discrepancies between the listed instui related
  * dependencies of a given package and it's typescript references listed in the package's tsconfig.build.json config.
  *
- * If there is any it will print out the differences to the console.
+ * If there is any it will print out the differences to the console and exits the current process with code 1.
  */
 const path = require('path')
 const chalk = require('chalk')
@@ -37,71 +37,69 @@ const fs = require('fs')
 const referencesFile = path.resolve('./tsconfig.references.json')
 const tsReferences = require(referencesFile).references
 
-const packageDepsAndRefs = tsReferences
-  .map((reference) => {
-    const [, dir, package] = reference.path.split('/')
-    const packageJson = require(path.resolve(
-      `./${dir}/${package}/package.json`
-    ))
-    const tsConfigJson = require(path.resolve(`${reference.path}`))
-    const { devDependencies, dependencies } = packageJson
+const packageDepsAndRefs = tsReferences.map((reference) => {
+  const [, dir, package] = reference.path.split('/')
+  const packageJson = require(path.resolve(`./${dir}/${package}/package.json`))
+  const tsConfigJson = require(path.resolve(`${reference.path}`))
+  const { devDependencies, dependencies } = packageJson
 
-    const packageReferences = new Set(
-      (tsConfigJson.references || []).map((obj) => {
-        const [, package] = obj.path.split('/')
+  const tsConfigReferencies = new Set(
+    (tsConfigJson.references || []).map((obj) => {
+      const [, package] = obj.path.split('/')
+
+      return package
+    })
+  )
+
+  const packageJsonDependencies = new Set(
+    Object.entries({ ...dependencies, ...devDependencies })
+      .filter(([depName]) => depName.includes('@instructure'))
+
+      .map(([depName]) => {
+        const [, package] = depName.split('/')
 
         return package
       })
-    )
-
-    const instuiDeps = new Set(
-      Object.entries({ ...dependencies, ...devDependencies })
-        .filter(([depName]) => depName.includes('@instructure'))
-
-        .map(([depName]) => {
-          const [, package] = depName.split('/')
-
-          return package
-        })
-        .filter((depName) => {
-          // check if the dependent package is a ts package at all
-          return fs.existsSync(
-            path.resolve(`./packages/${depName}/tsconfig.build.json`)
-          )
-        })
-    )
-
-    return {
-      package,
-      packageDepenecies: instuiDeps,
-      packageReferences
-    }
-  })
-  .filter(
-    ({ packageDepenecies, packageReferences }) =>
-      !eqaulSets(packageReferences, packageDepenecies)
+      .filter((depName) => {
+        // check if the dependent package is a ts package at all
+        return fs.existsSync(
+          path.resolve(`./packages/${depName}/tsconfig.build.json`)
+        )
+      })
   )
 
-if (packageDepsAndRefs.length > 0) {
-  for (let packageInfo of packageDepsAndRefs) {
-    const { packageDepenecies, packageReferences, package } = packageInfo
+  return {
+    package,
+    packageDependencies: packageJsonDependencies,
+    tsConfigReferencies
+  }
+})
 
-    console.warn(`Dependecy mismatch in package: ${chalk.red(package)}!`)
+const depsAndRefsWithDiff = packageDepsAndRefs.filter(
+  ({ packageDependencies, tsConfigReferencies }) =>
+    !equalSets(tsConfigReferencies, packageDependencies)
+)
+
+if (depsAndRefsWithDiff.length > 0) {
+  for (let packageInfo of depsAndRefsWithDiff) {
+    const { packageDependencies, tsConfigReferencies, package } = packageInfo
+
+    console.warn(`\n Dependency mismatch in package: ${chalk.red(package)}!`)
     console.warn(
-      `Please make sure the package.json 'dependecies' and 'devDependecies' entries and the tsconfig.build.json 'references' are synchronized!`
+      `Please make sure the package.json 'dependencies' and 'devDependencies' entries and the tsconfig.build.json 'references' are synchronized!`
     )
     console.warn(
       'These are the dependencies for the package:\n',
-      chalk.green([...packageDepenecies])
+      chalk.green([...packageDependencies])
     )
     console.warn(
       'And these are the project references listed in tsconfig.build.json:\n',
-      chalk.blue([...packageReferences])
+      chalk.blue([...tsConfigReferencies])
     )
     console.warn(
       'The difference is:\n',
       chalk.yellow([
-        ...symmetricDifference(packageDepenecies, packageReferences)
+        ...symmetricDifference(packageDependencies, tsConfigReferencies)
       ])
     )
   }
@@ -110,7 +108,7 @@ if (packageDepsAndRefs.length > 0) {
 
 process.exit(0)
 
-function eqaulSets(setA, setB) {
+function equalSets(setA, setB) {
   if (setA.size !== setB.size) return false
 
   for (let elem of setA) {
