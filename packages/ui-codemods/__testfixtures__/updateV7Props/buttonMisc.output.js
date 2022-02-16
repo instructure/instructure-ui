a9d6642f6d7bf7f9d425b9449fe73889c3a30673
Matyas Forian-Szabo <matyas.szabo@instructure.com>
2022-02-16 16:35:50 +0100

    refactor(ui-codemods): fix fluidWidth codemod for Buttons

--- packages/ui-codemods/__testfixtures__/updateV7Props/buttonMisc.output.js
+++ packages/ui-codemods/__testfixtures__/updateV7Props/buttonMisc.output.js
@@ -7,6 +7,6 @@ btn = <Button elementRef={() => {}} />
 btn = <Button />
 btn = <Button fluidWidth={btn} />
 btn = <Button display="block" textAlign="start" />
-btn = <Button display="block" textAlign="start" />
+btn = <Button someOtherAtt={4} display="block" textAlign="start" />
 btn = <Button display="block" textAlign="start" />
 btn = <Button renderIcon={SomeIcon} />
