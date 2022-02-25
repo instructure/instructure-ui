435c9ae794c15e2bd103f700f8c4e946d91c1b59
Viktor Ohad <viktor.ohad@instructure.com>
2022-02-25 14:28:01 +0100

    fix(ui-babel-preset): add back removed `@babel/plugin-proposal-optional-chaining`

--- packages/ui-babel-preset/lib/index.js
+++ packages/ui-babel-preset/lib/index.js
@@ -85,6 +85,7 @@ module.exports = function (
   }
   plugins = plugins.concat([
     require('babel-plugin-macros'),
+    require('@babel/plugin-proposal-optional-chaining').default,
     require('@babel/plugin-transform-destructuring').default,
     [require('@babel/plugin-proposal-decorators').default, { legacy: true }], // must run before plugins that set displayName!
     require('./babel-plugin-add-displayname-for-react'),
