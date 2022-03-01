cd4bdb15b9d85647491d8ea24d8b18e9b9785036
Viktor Ohad <viktor.ohad@instructure.com>
2022-03-01 16:13:25 +0100

    WIP: setup basic scripts to always get the latest snapshot

---
+++ examples/prepare.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+TMP_FILE=$(mktemp)
+echo "Retrieving Snapshot version..."
+#1. get the latest snapshot version
+SNAPSHOT_VERSION=$(npm view @instructure/ui --json | jq '."dist-tags".snapshot')
+echo "Snapshot version is: $SNAPSHOT_VERSION"
+
+#2. override package json with the new snapshot in a temp file
+jq ".devDependencies |= setpath([\"@instructure/ui\"]; $SNAPSHOT_VERSION)" <package.json >"$TMP_FILE"
+
+#3. copy over the tempfile content to override it
+cat "$TMP_FILE" >package.json
+rm -f "$TMP_FILE"
