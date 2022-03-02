64e5cde68fe092304ebc51cd49c4b2e5e1678c3f
Viktor Ohad <viktor.ohad@instructure.com>
2022-03-02 12:55:20 +0100

    WIP: add example app with instui components

---
+++ examples/esbuild.cjs
@@ -0,0 +1,54 @@
+/* eslint-disable no-console */
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 - present Instructure, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+;(async () => {
+  const esbuild = require('esbuild')
+
+  try {
+    const result = await esbuild.build({
+      entryPoints: ['./src/app.tsx'],
+      bundle: true,
+      // minify: true,
+      treeShaking: true,
+      platform: 'browser',
+      outfile: './public/bundle.js',
+      metafile: true,
+      jsx: 'transform',
+      loader: {
+        '.js': 'jsx'
+      },
+      define: {
+        'process.env.OMIT_INSTUI_DEPRECATION_WARNINGS': true,
+        global: false
+      }
+    })
+
+    const text = await esbuild.analyzeMetafile(result.metafile)
+    console.log(text)
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
+})()
