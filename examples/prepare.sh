38dda3503aedb6f804e9ccd8ba6d33afdc07b02b
Viktor Ohad <viktor.ohad@instructure.com>
2022-03-04 11:22:57 +0100

    chore: add regression testing workflow

--- examples/prepare.sh
+++ examples/prepare.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+# TODO: rewrite this to JavaScript...
 set -e
 TMP_FILE=$(mktemp)
-echo "Retrieving Snapshot version..."
-#1. get the latest snapshot version
+echo "Retrieving latest snapshot version..."
+# get the latest snapshot version
 SNAPSHOT_VERSION=$(npm view @instructure/ui --json | jq '."dist-tags".snapshot')
 echo "Snapshot version is: $SNAPSHOT_VERSION"
 
-#2. override package json with the new snapshot in a temp file
-jq ".dependencies |= setpath([\"@instructure/ui\"]; $SNAPSHOT_VERSION)" <package.json >"$TMP_FILE"
+# override package json with the snapshot version
+PKG_JSON=$(jq ".dependencies |= setpath([\"@instructure/ui\"]; $SNAPSHOT_VERSION)" <package.json)
+jq ".devDependencies |= setpath([\"@instructure/browserslist-config-instui\"]; $SNAPSHOT_VERSION)" <<<"$PKG_JSON" >"$TMP_FILE"
 
-#3. copy over the tempfile content to override it
+# copy over the temp file to the package.json
 cat "$TMP_FILE" >package.json
 rm -f "$TMP_FILE"
