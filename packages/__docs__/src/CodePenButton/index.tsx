47c327aa17f4bc37feccd23236833a2573cb0a21
Nam Nguyen <nam.nguyen@instructure.com>
2022-03-08 11:20:39 +0100

    refactor: fix the `placeholderImage`
    Closes: INSTUI-3428

--- packages/__docs__/src/CodePenButton/index.tsx
+++ packages/__docs__/src/CodePenButton/index.tsx
@@ -35,7 +35,8 @@ class CodePenButton extends Component<CodePenButtonProps> {
   static propTypes = propTypes
   static allowedProps = allowedProps
   static defaultProps = {
-    options: {}
+    options: {},
+    render: true
   }
   render() {
     // 2 choices:
@@ -55,9 +56,37 @@ class CodePenButton extends Component<CodePenButtonProps> {
     // Add component and icon imports
     // Add imports for mirrorHorizontalPlacement, DateTime, moment, debounce
     // And later we'll deal with the image imports and the preview bug
+    const importReact = `
+    import React from "react"
+    import ReactDOM from "react-dom"
+    import moment from 'moment'
+    import {avatarSquare, avatarPortrait, lorem} from "./samplemedia"
+    import {placeholderImage} from "./samplemedia"
+    import iconExample from "!svg-inline-loader!./heart_lg.svg"
+    `
+    const pattern = /<[A-Z]\w+|Icon\w+/gm
+    const neededClasses = this.props.code
+      .match(pattern)
+      ?.map((className) => className.replace('<', '').trim())
+    if (this.props.code.includes('Calendar')) {
+      neededClasses?.push('Calendar')
+    }
+    const uniqueClass = [...new Set(neededClasses)]
+    const { render } = this.props
+    const externalElements = this.props.code
+      .match(/class.\w+|function.\w+|const.\w+/gm)
+      ?.map((className) =>
+        className.replace(/class|function|const/gm, '').trim()
+      )
+    const asd = uniqueClass?.filter(
+      (className) => !externalElements?.includes(className)
+    )
+    const importClasses = `import {${asd.join(', ')}} from "@instructure/ui"\n`
+    const code = render ? `render(${this.props.code})` : this.props.code
+    const renderStatement = `const render = (el) => { ReactDOM.render(el, document.getElementById('app')) }\n`
     const data = {
       title: this.props.title,
-      js: this.props.code,
+      js: importReact + importClasses + renderStatement + code,
       private: true,
       editors: '001',
       html: '<div id="app"></div><div id="flash-messages"></div><div id="nav"></div>',
@@ -74,12 +103,16 @@ class CodePenButton extends Component<CodePenButtonProps> {
         '@instructure/debounce': '^8',
         '@instructure/ui': '^8',
         '@instructure/ui-icons': '^8',
-        'lorem-ipsum': '2.0.4',
+        'lorem-ipsum': '^1.0.0',
         react: '17.0.2',
         'react-dom': '17.0.2',
-        'react-scripts': '4.0.0'
+        'react-scripts': '4.0.0',
+        moment: '^2.23.0',
+        'svg-inline-loader': '0.8.2'
       }
     }).replace(/'/g, '&apos;')
+    const IconSVG = `<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='{{w}}' height='{{h}}' viewBox='0 0 {{w}} {{h}}'><defs><symbol id='a' viewBox='0 0 90 66' opacity='0.3'><path d='M85 5v56H5V5h80m5-5H0v66h90V0z'/><circle cx='18' cy='20' r='6'/><path d='M56 14L37 39l-8-6-17 23h67z'/></symbol></defs><rect x='0' y='0' fill='#F8F8F8' width='100%' height='100%'/><text x='10' y='20' fill='#ccc' style="font: 14px sans-serif;">FPO: {{w}} x {{h}}</text><use xlink:href='#a' width='20%' x='40%'/></svg>
+    `
     const parameters = getParameters({
       files: {
         'package.json': {
@@ -93,6 +126,56 @@ class CodePenButton extends Component<CodePenButtonProps> {
         'index.html': {
           content: data.html,
           isBinary: false
+        },
+        'samplemedia.js': {
+          content: `import loremgen from "lorem-ipsum"
+          const IconSVG = \`${IconSVG}\`
+          export const avatarSquare = "https://raw.githubusercontent.com/instructure/instructure-ui/master/packages/__docs__/buildScripts/samplemedia/avatarSquare.jpg"
+          export const avatarPortrait = "https://raw.githubusercontent.com/instructure/instructure-ui/master/packages/__docs__/buildScripts/samplemedia/avatarPortrait.jpg"
+          export const lorem ={
+            sentence() {
+              return loremgen({
+                count: 1,
+                units: 'sentences'
+              })
+            },
+            paragraph() {
+              return loremgen({
+                count: 1,
+                units: 'paragraphs'
+              })
+            },
+            paragraphs(count) {
+              return loremgen({
+                count: count || Math.floor(Math.random() * 10),
+                units: 'paragraphs'
+              })
+            }
+          }
+          export function placeholderImage(width = 512, height = 512) {
+            // We need to base64 encode this because otherwise FF will add extra escape chars
+            const dataUri = btoa(
+              IconSVG.replace(/{{w}}/g, width).replace(/{{h}}/g, height).trim()
+            )
+            return \`data:image/svg+xml;base64,\${dataUri}\`
+          }
+          `,
+          isBinary: false
+        },
+        'placeholder-image.js': {
+          content:
+            'https://raw.githubusercontent.com/instructure/instructure-ui/master/packages/__docs__/buildScripts/samplemedia/placeholder-image.js',
+          isBinary: true
+        },
+        'placeholder.svg': {
+          content:
+            'https://raw.githubusercontent.com/instructure/instructure-ui/master/packages/__docs__/buildScripts/samplemedia/placeholder.svg',
+          isBinary: true
+        },
+        'heart_lg.svg': {
+          content:
+            'https://raw.githubusercontent.com/instructure/instructure-ui/master/packages/__docs__/buildScripts/samplemedia/heart_lg.svg',
+          isBinary: true
         }
       }
     })
