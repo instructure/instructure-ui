ef01885bad9c15ea579a0607db53aff433bf9f30
Nam Nguyen <nam.nguyen@instructure.com>
2022-03-09 09:16:52 +0100

    refactor: fix PR of package.json and globals.js
    Closes: INSTUI-3428

--- packages/__docs__/src/CodePenButton/index.tsx
+++ packages/__docs__/src/CodePenButton/index.tsx
@@ -111,8 +111,7 @@ class CodePenButton extends Component<CodePenButtonProps> {
         'svg-inline-loader': '0.8.2'
       }
     }).replace(/'/g, '&apos;')
-    const IconSVG = `<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='{{w}}' height='{{h}}' viewBox='0 0 {{w}} {{h}}'><defs><symbol id='a' viewBox='0 0 90 66' opacity='0.3'><path d='M85 5v56H5V5h80m5-5H0v66h90V0z'/><circle cx='18' cy='20' r='6'/><path d='M56 14L37 39l-8-6-17 23h67z'/></symbol></defs><rect x='0' y='0' fill='#F8F8F8' width='100%' height='100%'/><text x='10' y='20' fill='#ccc' style="font: 14px sans-serif;">FPO: {{w}} x {{h}}</text><use xlink:href='#a' width='20%' x='40%'/></svg>
-    `
+    const IconSVG = `<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='{{w}}' height='{{h}}' viewBox='0 0 {{w}} {{h}}'><defs><symbol id='a' viewBox='0 0 90 66' opacity='0.3'><path d='M85 5v56H5V5h80m5-5H0v66h90V0z'/><circle cx='18' cy='20' r='6'/><path d='M56 14L37 39l-8-6-17 23h67z'/></symbol></defs><rect x='0' y='0' fill='#F8F8F8' width='100%' height='100%'/><text x='10' y='20' fill='#ccc' style="font: 14px sans-serif;">FPO: {{w}} x {{h}}</text><use xlink:href='#a' width='20%' x='40%'/></svg>`
     const parameters = getParameters({
       files: {
         'package.json': {
@@ -128,38 +127,39 @@ class CodePenButton extends Component<CodePenButtonProps> {
           isBinary: false
         },
         'samplemedia.js': {
-          content: `import loremgen from "lorem-ipsum"
-          const IconSVG = \`${IconSVG}\`
-          export const avatarSquare = "https://raw.githubusercontent.com/instructure/instructure-ui/master/packages/__docs__/buildScripts/samplemedia/avatarSquare.jpg"
-          export const avatarPortrait = "https://raw.githubusercontent.com/instructure/instructure-ui/master/packages/__docs__/buildScripts/samplemedia/avatarPortrait.jpg"
-          export const lorem ={
-            sentence() {
-              return loremgen({
-                count: 1,
-                units: 'sentences'
-              })
-            },
-            paragraph() {
-              return loremgen({
-                count: 1,
-                units: 'paragraphs'
-              })
-            },
-            paragraphs(count) {
-              return loremgen({
-                count: count || Math.floor(Math.random() * 10),
-                units: 'paragraphs'
-              })
-            }
-          }
-          export function placeholderImage(width = 512, height = 512) {
-            // We need to base64 encode this because otherwise FF will add extra escape chars
-            const dataUri = btoa(
-              IconSVG.replace(/{{w}}/g, width).replace(/{{h}}/g, height).trim()
-            )
-            return \`data:image/svg+xml;base64,\${dataUri}\`
-          }
-          `,
+          content: `
+import loremgen from "lorem-ipsum"
+const IconSVG = \`${IconSVG}\`
+export const avatarSquare = "https://raw.githubusercontent.com/instructure/instructure-ui/master/packages/__docs__/buildScripts/samplemedia/avatarSquare.jpg"
+export const avatarPortrait = "https://raw.githubusercontent.com/instructure/instructure-ui/master/packages/__docs__/buildScripts/samplemedia/avatarPortrait.jpg"
+export const lorem ={
+  sentence() {
+    return loremgen({
+      count: 1,
+      units: 'sentences'
+    })
+  },
+  paragraph() {
+    return loremgen({
+      count: 1,
+      units: 'paragraphs'
+    })
+  },
+  paragraphs(count) {
+    return loremgen({
+      count: count || Math.floor(Math.random() * 10),
+      units: 'paragraphs'
+    })
+  }
+}
+export function placeholderImage(width = 512, height = 512) {
+  // We need to base64 encode this because otherwise FF will add extra escape chars
+  const dataUri = btoa(
+    IconSVG.replace(/{{w}}/g, width).replace(/{{h}}/g, height).trim()
+  )
+  return \`data:image/svg+xml;base64,\${dataUri}\`
+}
+`,
           isBinary: false
         },
         'placeholder-image.js': {
