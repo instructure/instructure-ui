a7deafe45a9602481e56d51ece1da3b105ffcc90
Matyas Forian-Szabo <matyas.szabo@instructure.com>
2022-03-16 10:48:30 +0100

    refactor(ui-codemods): fix List codemod for updating v7 props

--- packages/ui-codemods/__testfixtures__/updateV7Props/list.input.js
+++ packages/ui-codemods/__testfixtures__/updateV7Props/list.input.js
@@ -1,12 +1,12 @@
 /* eslint-disable */
-import { List } from '@instructure/ui-lists'
+import { List } from '@instructure/ui-list'
 
 const aVar = {}
 const a = (
   <p>
     <List variant="default"></List>
     <List variant="unstyled"></List>
-    <List variant="inline" delimiter="," >
+    <List variant="inline" delimiter="dashed" >
       <List.Item>test</List.Item>
       {...aVar}
     </List>
