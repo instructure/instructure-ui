b68e1d19cc9fbefd7c7d3f4bceb6539485c140c6
NamNguyenInstructure <nam.nguyen@instructure.com>
2022-03-21 11:24:49 +0100

    refactor(ui-rating): revert deleted lines
    Closes: INSTUI-3076

--- packages/ui-rating/src/RatingIcon/__tests__/RatingIcon.test.tsx
+++ packages/ui-rating/src/RatingIcon/__tests__/RatingIcon.test.tsx
@@ -26,8 +26,9 @@ import React from 'react'
 import { expect, mount, within } from '@instructure/ui-test-utils'
 
 import { InstUISettingsProvider } from '@instructure/emotion'
-
-import { TransitionLocator } from '@instructure/ui-motion/src/Transition/TransitionLocator'
+// @ts-expect-error ts-migrate(7016) FIXME: Could not find a declaration file for module '@ins... Remove this comment to see the full error message
+// eslint-disable-next-line no-restricted-imports
+import { TransitionLocator } from '@instructure/ui-motion/es/Transition/TransitionLocator'
 
 import { RatingIcon } from '../index'
 
