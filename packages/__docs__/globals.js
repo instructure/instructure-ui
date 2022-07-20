7ef62823dff36ce1b4855a2e5ffd9625e373685d
Nam Nguyen <nam.nguyen@instructure.com>
2022-07-20 16:32:45 +0700

    fix(ui-webpack-config): `iconExample` is missing from SVGIcon and InlineSVG docs pages

--- packages/__docs__/globals.js
+++ packages/__docs__/globals.js
@@ -43,7 +43,7 @@ import '@instructure/ui-icons/es/font/Line/InstructureIcons-Line.css'
 import { DateTime } from '@instructure/ui-i18n'
 
 // eslint-disable-next-line import/no-unresolved
-import iconExample from '!svg-inline-loader!./buildScripts/samplemedia/heart_lg.svg'
+import iconExample from './buildScripts/samplemedia/heart_lg.svg'
 import avatarSquare from './buildScripts/samplemedia/avatarSquare.jpg'
 import avatarPortrait from './buildScripts/samplemedia/avatarPortrait.jpg'
 import placeholderImage from './buildScripts/samplemedia/placeholder-image'
