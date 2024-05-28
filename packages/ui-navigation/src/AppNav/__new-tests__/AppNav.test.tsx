03a63deccd0a42fa39d25041059fb541530be4d5
Nandor_Czegledi <nandor.czegledi@instructure.com>
2024-05-28 16:51:41 +0200

    test(ui-navigation): migrate old Navigation tests
    Closes: INSTUI-4075

--- packages/ui-navigation/src/AppNav/__new-tests__/AppNav.test.tsx
+++ packages/ui-navigation/src/AppNav/__new-tests__/AppNav.test.tsx
@@ -22,23 +22,35 @@
  * SOFTWARE.
  */
 import React from 'react'
-import {
-  expect,
-  mount,
-  generateA11yTests,
-  stub,
-  wait,
-  within
-} from '@instructure/ui-test-utils'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
 
-import AppNavExamples from '../__examples__/AppNav.examples'
+// eslint-disable-next-line no-restricted-imports
+import { generateA11yTests } from '@instructure/ui-scripts/lib/test/generateA11yTests'
+import { runAxeCheck } from '@instructure/ui-axe-check'
+
 import { AppNav } from '../index'
-import { AppNavLocator } from '../AppNavLocator'
+import AppNavExamples from '../__examples__/AppNav.examples'
 
-describe('<AppNav />', async () => {
+const originalResizeObserver = global.ResizeObserver
+
+describe('<AppNav />', () => {
+  beforeAll(() => {
+    // Mock for ResizeObserver browser API
+    global.ResizeObserver = jest.fn().mockImplementation(() => ({
+      observe: jest.fn(),
+      unobserve: jest.fn(),
+      disconnect: jest.fn()
+    }))
+  })
+
+  afterAll(() => {
+    global.ResizeObserver = originalResizeObserver
+  })
+
   describe('for a11y', () => {
     it('should render a nav element with an aria-label', async () => {
-      await mount(
+      const { container } = render(
         <AppNav screenReaderLabel="Screen reader label" visibleItemsCount={2}>
           <AppNav.Item
             renderLabel="Some label"
@@ -51,12 +63,15 @@ describe('<AppNav />', async () => {
         </AppNav>
       )
 
-      const nav = await AppNavLocator.find()
-      expect(await nav.find('ul[aria-label="Screen reader label"]')).to.exist()
+      const appNavList = container.querySelector('ul[class$="-appNav__list"]')!
+
+      expect(appNavList).toBeInTheDocument()
+      expect(appNavList.tagName).toBe('UL')
+      expect(appNavList).toHaveAttribute('aria-label', 'Screen reader label')
     })
 
     it('should render a semantic list of items', async () => {
-      await mount(
+      render(
         <AppNav screenReaderLabel="App navigation" visibleItemsCount={2}>
           <AppNav.Item
             renderLabel="Some first label"
@@ -69,21 +84,19 @@ describe('<AppNav />', async () => {
         </AppNav>
       )
 
-      const nav = await AppNavLocator.find()
+      const list = screen.getAllByRole('list')
+      const items = screen.getAllByRole('listitem')
+      const link = screen.getAllByRole('link')
+      const button = screen.getAllByRole('button')
 
-      const list = await nav.findAll('ul')
-      const items = await nav.findAll('li')
-      const link = await nav.findAll('a')
-      const button = await nav.findAll('button')
-
-      expect(list).to.have.length(1)
-      expect(items).to.have.length(2)
-      expect(link).to.have.length(1)
-      expect(button).to.have.length(1)
+      expect(list.length).toBe(1)
+      expect(items.length).toBe(2)
+      expect(link.length).toBe(1)
+      expect(button.length).toBe(1)
     })
 
     it('should render with a single item', async () => {
-      await mount(
+      render(
         <AppNav screenReaderLabel="App navigation" visibleItemsCount={1}>
           <AppNav.Item
             renderLabel="Some label"
@@ -92,21 +105,19 @@ describe('<AppNav />', async () => {
         </AppNav>
       )
 
-      const nav = await AppNavLocator.find()
+      const list = screen.getAllByRole('list')
+      const items = screen.getAllByRole('listitem')
+      const link = screen.getAllByRole('link')
 
-      const list = await nav.findAll('ul')
-      const items = await nav.findAll('li')
-      const link = await nav.findAll('a')
-
-      expect(list).to.have.length(1)
-      expect(items).to.have.length(1)
-      expect(link).to.have.length(1)
+      expect(list.length).toBe(1)
+      expect(items.length).toBe(1)
+      expect(link.length).toBe(1)
     })
   })
 
   describe('with rendered content', () => {
     it('should render content after the navigation', async () => {
-      await mount(
+      render(
         <AppNav
           screenReaderLabel="App navigation"
           renderAfterItems={<button type="button">I am rendered after!</button>}
@@ -122,55 +133,15 @@ describe('<AppNav />', async () => {
           />
         </AppNav>
       )
-      const nav = await AppNavLocator.find()
-      expect(await nav.find('button:contains(I am rendered after!)')).to.exist()
+      const button = screen.getByRole('button')
+
+      expect(button).toHaveTextContent('I am rendered after!')
     })
   })
 
   describe('with item truncation', () => {
-    it('should hide and show items based on the containing element width', async () => {
-      const onUpdate = stub()
-      const itemWidth = 70
-
-      const Nav = ({ width }: { width: number }) => (
-        <div style={{ width }}>
-          <AppNav screenReaderLabel="App navigation" onUpdate={onUpdate}>
-            <AppNav.Item
-              renderLabel={<div style={{ width: itemWidth }}>coolLabel</div>}
-              href="http://instructure.design"
-            />
-            <AppNav.Item
-              renderLabel={<div style={{ width: itemWidth }}>coolLabel</div>}
-              href="http://instructure.design"
-            />
-            <AppNav.Item
-              renderLabel={<div style={{ width: itemWidth }}>coolLabel</div>}
-              href="http://instructure.design"
-            />
-            <AppNav.Item
-              renderLabel={<div style={{ width: itemWidth }}>coolLabel</div>}
-              href="http://instructure.design"
-            />
-          </AppNav>
-        </div>
-      )
-
-      const subject = await mount(<Nav width={800} />)
-
-      await wait(() => {
-        expect(onUpdate).to.have.been.calledWith({ visibleItemsCount: 4 })
-      })
-
-      onUpdate.reset()
-      await subject.setProps({ width: 400 })
-
-      await wait(() => {
-        expect(onUpdate).to.have.been.calledWith({ visibleItemsCount: 2 })
-      })
-    })
-
     it('should pass a custom label to the menu trigger', async () => {
-      const subject = await mount(
+      render(
         <AppNav
           screenReaderLabel="App navigation"
           visibleItemsCount={2}
@@ -185,14 +156,21 @@ describe('<AppNav />', async () => {
         </AppNav>
       )
 
-      const nav = within(subject.getDOMNode())
-      const trigger = await nav.findAll('li:contains(I am sooo custom!)')
+      const items = screen.getAllByRole('listitem')
 
-      expect(trigger).to.have.length(1)
+      expect(items.length).toBe(3)
+      expect(items[2]).toHaveTextContent('I am sooo custom!')
     })
   })
 
-  describe('with generated examples', async () => {
-    generateA11yTests(AppNav, AppNavExamples)
+  describe('with generated examples', () => {
+    const generatedComponents = generateA11yTests(AppNav, AppNavExamples)
+    for (const component of generatedComponents) {
+      it(component.description, async () => {
+        const { container } = render(component.content)
+        const axeCheck = await runAxeCheck(container)
+        expect(axeCheck).toBe(true)
+      })
+    }
   })
 })
