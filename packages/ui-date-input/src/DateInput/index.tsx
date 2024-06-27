2d2309e51ae7acb4088ce62e84e17f9fde8b43ce
Balázs Sáros <balazs.saros@instructure.com>
2024-06-27 16:08:43 +0200

    WIP(ui-date-input): add new version for DateInput

--- packages/ui-date-input/src/DateInput/index.tsx
+++ packages/ui-date-input/src/DateInput/index.tsx
@@ -23,8 +23,15 @@
  */
 
 /** @jsx jsx */
-import React, { Children, Component, ReactElement } from 'react'
+import React, {
+  Children,
+  Component,
+  ReactElement,
+  useState,
+  useEffect
+} from 'react'
 
+import moment from 'moment-timezone'
 import { Calendar } from '@instructure/ui-calendar'
 import { IconButton } from '@instructure/ui-buttons'
 import type { CalendarProps, CalendarDayProps } from '@instructure/ui-calendar'
@@ -65,7 +72,7 @@ See <https://instructure.design/#DateInput/>
 **/
 @withStyle(generateStyle, null)
 @testable()
-class DateInput extends Component<DateInputProps, DateInputState> {
+class _DateInput extends Component<DateInputProps, DateInputState> {
   static readonly componentId = 'DateInput'
   static Day = Calendar.Day
   declare context: React.ContextType<typeof ApplyLocaleContext>
@@ -509,6 +516,154 @@ class DateInput extends Component<DateInputProps, DateInputState> {
       </Selectable>
     )
   }
+}
+
+const DateInput = ({
+  renderLabel,
+  isRequired,
+  value,
+  width,
+  onChange,
+  onBlur,
+  withYearPicker,
+  invalidDateErrorMessage,
+  locale,
+  timezone,
+  size,
+  placeholder,
+  isInline
+}: DateInputProps) => {
+  const [selectedDate, setSelectedDate] = useState<string>('')
+  const [inputMessages, setInputMessages] = useState<FormMessage[]>([])
+  const [showPopover, setShowPopover] = useState<boolean>(false)
+
+  useEffect(() => {
+    validateInput(true)
+  }, [value])
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    value: string
+  ) => {
+    onChange?.(e, { value })
+  }
+
+  const handleDateSelected = (dateString: string, momentDate: any, e: any) => {
+    setSelectedDate(dateString)
+    handleInputChange(
+      e,
+      `${momentDate.format('MMMM')} ${momentDate.format(
+        'D'
+      )}, ${momentDate.format('YYYY')}`
+    )
+    setShowPopover(false)
+  }
+
+  const validateInput = (onlyRemoveError = false) => {
+    if (
+      moment
+        .tz(
+          value ? value : '',
+          [
+            DateTime.momentISOFormat,
+            'llll',
+            'LLLL',
+            'lll',
+            'LLL',
+            'll',
+            'LL',
+            'l',
+            'L'
+          ],
+          getLocale(),
+          true,
+          getTimezone()
+        )
+        .isValid() ||
+      value === ''
+    ) {
+      setInputMessages([])
+      return
+    }
+    if (typeof invalidDateErrorMessage !== 'function' && !onlyRemoveError) {
+      setInputMessages([
+        {
+          type: 'error',
+          text: invalidDateErrorMessage
+        }
+      ])
+    }
+  }
+
+  const getLocale = () => {
+    if (locale) {
+      return locale
+    }
+    // TODO make it work with context
+    // } else if (this.context && this.context.locale) {
+    //   return this.context.locale
+    // }
+    return Locale.browserLocale()
+  }
+
+  const getTimezone = () => {
+    if (timezone) {
+      return timezone
+    }
+    // TODO make it work with context
+    // } else if (this.context && this.context.timezone) {
+    //   return this.context.timezone
+    // }
+    return DateTime.browserTimeZone()
+  }
+
+  const handleBlur = (e: React.SyntheticEvent) => {
+    onBlur?.(e)
+    validateInput(false)
+  }
+
+  return (
+    <TextInput
+      renderLabel={renderLabel}
+      onChange={handleInputChange}
+      onBlur={handleBlur}
+      isRequired={isRequired}
+      value={value}
+      placeholder={placeholder}
+      width={width}
+      size={size}
+      display={isInline ? 'inline-block' : 'block'}
+      messages={inputMessages}
+      renderAfterInput={
+        <Popover
+          renderTrigger={
+            <IconButton
+              withBackground={false}
+              withBorder={false}
+              screenReaderLabel="Calendar"
+              shape="circle"
+            >
+              <IconCalendarMonthLine inline={false} />
+            </IconButton>
+          }
+          isShowingContent={showPopover}
+          onShowContent={() => setShowPopover(true)}
+          onHideContent={() => setShowPopover(false)}
+          on="click"
+          // screenReaderLabel={this.props.popoverScreenReaderLabel} TODO
+          shouldContainFocus
+          shouldReturnFocus
+          shouldCloseOnDocumentClick
+        >
+          <Calendar
+            withYearPicker={withYearPicker}
+            onDateSelected={handleDateSelected}
+            selectedDate={selectedDate}
+          />
+        </Popover>
+      }
+    />
+  )
 }
 
 export default DateInput
