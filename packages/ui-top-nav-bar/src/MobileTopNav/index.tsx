36c03c601aff0b8ec99c1b3fea565e0b26893838
Balázs Sáros <balazs.saros@instructure.com>
2024-08-22 16:40:04 +0200

    WIP(ui-top-nav-bar): wip

--- packages/ui-top-nav-bar/src/MobileTopNav/index.tsx
+++ packages/ui-top-nav-bar/src/MobileTopNav/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/display-name */
 /*
  * The MIT License (MIT)
  *
@@ -23,18 +24,13 @@
  */
 
 /** @jsx jsx */
-// import { useState, useEffect, useContext } from 'react'
-
-import { useState } from 'react'
+import { Children, Fragment, useState, useEffect } from 'react'
+import type { PropsWithChildren } from 'react'
 import { jsx, useTheme } from '@instructure/emotion'
-import type { MobileTopNavProps, MobileTopNavOwnProps } from './props'
+import type { MobileTopNavProps } from './props'
 
 import { IconButton } from '@instructure/ui-buttons'
-import {
-  IconHamburgerSolid,
-  IconEyeSolid,
-  IconXSolid
-} from '@instructure/ui-icons'
+import { IconHamburgerLine, IconXLine } from '@instructure/ui-icons'
 
 /**
 ---
@@ -44,35 +40,28 @@ category: components
 const MobileTopNav = ({
   lightMode = false,
   brand,
-  styles
+  styles,
+  children
 }: MobileTopNavProps) => {
   const [open, setOpen] = useState<boolean>(false)
 
+  useEffect(() => {
+    const body = document.getElementsByTagName('body')[0]
+    body.style.overflow = open ? 'hidden' : 'visible'
+  }, [open])
+
+  const getSubComponent = (displayName: any) => {
+    return Children.map(children, (child: any) => child).filter(
+      (child: any) => child?.type?.displayName === displayName
+    )
+  }
+
   return (
-    <div css={{ ...styles.container, ...styles.containerHeight(open) }}>
-      <div
-        css={{
-          height: '54px',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-between'
-        }}
-      >
+    <div css={styles.container(open)}>
+      <div css={styles.topBar}>
         {brand}
-        <span
-          css={{
-            display: 'flex',
-            gap: '12px'
-          }}
-        >
-          <IconButton
-            withBackground={false}
-            withBorder={false}
-            screenReaderLabel="burgir"
-            color={lightMode ? 'secondary' : 'primary-inverse'}
-          >
-            <IconEyeSolid />
-          </IconButton>
+        <span css={styles.btnRow}>
+          {!open && getSubComponent('BtnRow')}
           <IconButton
             withBackground={false}
             withBorder={false}
@@ -80,42 +69,151 @@ const MobileTopNav = ({
             color={lightMode ? 'secondary' : 'primary-inverse'}
             onClick={() => setOpen((open) => !open)}
           >
-            {open ? <IconXSolid /> : <IconHamburgerSolid />}
+            {open ? <IconXLine /> : <IconHamburgerLine />}
           </IconButton>
         </span>
+      </div>
+
+      <div style={styles.content(open)}>
+        {getSubComponent('BreadCrumb')}
+        {getSubComponent('Title')}
+        <p>
+          1 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias
+          excepturi a blanditiis, aspernatur repellat repellendus dolores cum
+          labore eligendi architecto asperiores, dolor quisquam sequi mollitia
+          quibusdam, cumque id ab amet?
+        </p>
+        <p>
+          2 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias
+          excepturi a blanditiis, aspernatur repellat repellendus dolores cum
+          labore eligendi architecto asperiores, dolor quisquam sequi mollitia
+          quibusdam, cumque id ab amet?
+        </p>
+        <p>
+          3 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias
+          excepturi a blanditiis, aspernatur repellat repellendus dolores cum
+          labore eligendi architecto asperiores, dolor quisquam sequi mollitia
+          quibusdam, cumque id ab amet?
+        </p>
+        <p>
+          4 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias
+          excepturi a blanditiis, aspernatur repellat repellendus dolores cum
+          labore eligendi architecto asperiores, dolor quisquam sequi mollitia
+          quibusdam, cumque id ab amet?
+        </p>
+        <p>
+          5 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias
+          excepturi a blanditiis, aspernatur repellat repellendus dolores cum
+          labore eligendi architecto asperiores, dolor quisquam sequi mollitia
+          quibusdam, cumque id ab amet?
+        </p>
+        <p>
+          6 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias
+          excepturi a blanditiis, aspernatur repellat repellendus dolores cum
+          labore eligendi architecto asperiores, dolor quisquam sequi mollitia
+          quibusdam, cumque id ab amet?
+        </p>
+        <p>
+          7 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias
+          excepturi a blanditiis, aspernatur repellat repellendus dolores cum
+          labore eligendi architecto asperiores, dolor quisquam sequi mollitia
+          quibusdam, cumque id ab amet?
+        </p>
+        <p>
+          8 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias
+          excepturi a blanditiis, aspernatur repellat repellendus dolores cum
+          labore eligendi architecto asperiores, dolor quisquam sequi mollitia
+          quibusdam, cumque id ab amet?
+        </p>
+        <p>
+          9 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias
+          excepturi a blanditiis, aspernatur repellat repellendus dolores cum
+          labore eligendi architecto asperiores, dolor quisquam sequi mollitia
+          quibusdam, cumque id ab amet?
+        </p>
+        <p>
+          10 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias
+          excepturi a blanditiis, aspernatur repellat repellendus dolores cum
+          labore eligendi architecto asperiores, dolor quisquam sequi mollitia
+          quibusdam, cumque id ab amet?
+        </p>
+        <p>
+          11 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias
+          excepturi a blanditiis, aspernatur repellat repellendus dolores cum
+          labore eligendi architecto asperiores, dolor quisquam sequi mollitia
+          quibusdam, cumque id ab amet?
+        </p>
       </div>
     </div>
   )
 }
 
-const generateStyles = (props, theme) => {
+const generateStyles = (props: MobileTopNavProps, theme: any) => {
   const { lightMode } = props
   return {
-    container: {
-      padding: `0 ${theme.spacing.small}`,
-      backgroundColor: lightMode
-        ? theme.colors.ui.surfacePageSecondary
-        : theme.colors.ui.surfaceDark,
-      color: lightMode
-        ? theme.colors.contrasts.grey125125
-        : theme.colors?.contrasts?.white1010,
-      position: 'absolute',
-      top: 0,
-      bottom: 0,
-      left: 0,
-      right: 0,
-      width: '100%'
-    },
-    containerHeight: (open: boolean) => {
+    container: (open: boolean) => {
       return {
-        height: open ? '100%' : '54px',
-        position: open ? 'absolute' : 'relative'
+        height: '54px',
+        position: open ? 'fixed' : 'relative',
+        backgroundColor: lightMode
+          ? theme.colors.ui.surfacePageSecondary
+          : theme.colors.ui.surfaceDark,
+        color: lightMode
+          ? theme.colors.contrasts.grey125125
+          : theme.colors?.contrasts?.white1010,
+        width: '100%'
       }
+    },
+    topBar: {
+      padding: `0 ${theme.spacing.small}`,
+      height: '54px',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between'
+    },
+    content: (open: boolean) => {
+      return {
+        padding: `0 ${theme.spacing.small}`,
+        height: open ? '100%' : '0px',
+        top: '54px',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        overflow: open ? 'scroll' : 'hidden',
+        position: 'fixed',
+        backgroundColor: lightMode
+          ? theme.colors.ui.surfacePageSecondary
+          : theme.colors.ui.surfaceDark,
+        color: lightMode
+          ? theme.colors.contrasts.grey125125
+          : theme.colors?.contrasts?.white1010
+      }
+    },
+    btnRow: {
+      display: 'flex',
+      gap: '12px'
     }
   }
 }
 
-// eslint-disable-next-line react/display-name
+const BtnRow = ({ children }: PropsWithChildren) => {
+  return <Fragment>{children}</Fragment>
+}
+
+BtnRow.displayName = 'BtnRow'
+
+const BreadCrumb = ({ children }: PropsWithChildren) => {
+  return <div css={{ margin: '24px 0' }}>{children}</div>
+}
+
+BreadCrumb.displayName = 'BreadCrumb'
+
+const Title = ({ children }: PropsWithChildren) => {
+  return <div css={{ margin: '32px 0' }}>{children}</div>
+}
+
+Title.displayName = 'Title'
+
 const withStyles =
   <ComponentOwnProps, ComponentStyle>(
     generateStyles: (props: any, theme: any) => ComponentStyle
@@ -130,7 +228,11 @@ const withStyles =
     return <WrappedComponent {...styledProps} />
   }
 
-const SC = withStyles(generateStyles)(MobileTopNav)
+const SC: any = withStyles(generateStyles)(MobileTopNav)
+
+SC.BtnRow = BtnRow
+SC.BreadCrumb = BreadCrumb
+SC.Title = Title
 
 export { SC as MobileTopNav }
 export default SC
