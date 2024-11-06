63ab71926c7711ac085ad061baf586de449ad937
Nandor_Czegledi <nandor.czegledi@instructure.com>
2024-11-06 17:22:15 +0100

    test(ui-calendar): migrate old Calendar tests
    Closes: INSTUI-4341

---
+++ packages/ui-calendar/src/Calendar/Day/__new-tests__/Day.test.tsx
@@ -0,0 +1,303 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 - present Instructure, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import React from 'react'
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { vi } from 'vitest'
+import '@testing-library/jest-dom'
+
+// eslint-disable-next-line no-restricted-imports
+import { generateA11yTests } from '@instructure/ui-scripts/lib/test/generateA11yTests'
+import { runAxeCheck } from '@instructure/ui-axe-check'
+import { Day } from '../index'
+import DayExamples from '../__examples__/Day.examples'
+
+describe('Day', () => {
+  it('should render children', async () => {
+    const { rerender } = render(
+      <Day date="2019-08-02" label="1 August 2019 Friday">
+        8
+      </Day>
+    )
+    const child = screen.getByText('8')
+
+    expect(child).toBeInTheDocument()
+
+    rerender(
+      <Day date="2019-08-02" label="1 August 2019 Friday">
+        31
+      </Day>
+    )
+    const childUpdated = screen.getByText('31')
+
+    expect(childUpdated).toBeInTheDocument()
+  })
+
+  it('should have an accessible label', async () => {
+    const label = '1 August 2019 Friday'
+    const { container } = render(
+      <Day date="2019-08-02" label={label}>
+        8
+      </Day>
+    )
+    const screenReaderContent = container.querySelector(
+      '[class$="-screenReaderContent"]'
+    )
+
+    expect(screenReaderContent).toBeInTheDocument()
+    expect(screenReaderContent).toHaveTextContent(label)
+  })
+
+  it('should set aria-current="date" when `isToday`', async () => {
+    const { container, rerender } = render(
+      <Day date="2019-08-02" label="1 August 2019 Friday" isToday>
+        8
+      </Day>
+    )
+    const today = container.querySelector('[class$="-calendarDay"]')
+
+    expect(today).toHaveAttribute('aria-current', 'date')
+
+    rerender(
+      <Day date="2019-08-02" label="1 August 2019 Friday" isToday={false}>
+        8
+      </Day>
+    )
+    const day = container.querySelector('[class$="-calendarDay"]')
+
+    expect(day).not.toHaveAttribute('aria-current', 'date')
+  })
+
+  it('should not set aria-selected without a role', async () => {
+    const { container, rerender } = render(
+      <Day date="2019-08-02" label="1 August 2019 Friday">
+        8
+      </Day>
+    )
+    const day = container.querySelector('[class$="-calendarDay"]')
+
+    expect(day).not.toHaveAttribute('aria-selected')
+
+    rerender(
+      <Day date="2019-08-02" label="1 August 2019 Friday" isSelected={true}>
+        8
+      </Day>
+    )
+    const daySelected = container.querySelector('[class$="-calendarDay"]')
+
+    expect(daySelected).not.toHaveAttribute('aria-selected')
+  })
+
+  it('should set aria-selected="true/false" when `isSelected` and `role` is `option` or `gridcell`', async () => {
+    const { container, rerender } = render(
+      <Day date="2019-08-02" label="1 August 2019 Friday" role="option">
+        8
+      </Day>
+    )
+    const day = container.querySelector('[class$="-calendarDay"]')
+    expect(day).toHaveAttribute('aria-selected', 'false')
+
+    rerender(
+      <Day
+        date="2019-08-02"
+        label="1 August 2019 Friday"
+        role="option"
+        isSelected={true}
+      >
+        8
+      </Day>
+    )
+    const daySelected = container.querySelector('[class$="-calendarDay"]')
+    expect(daySelected).toHaveAttribute('aria-selected', 'true')
+
+    rerender(
+      <Day
+        date="2019-08-02"
+        label="1 August 2019 Friday"
+        role="gridcell"
+        isSelected={false}
+      >
+        8
+      </Day>
+    )
+    const dayCell = container.querySelector('[class$="-calendarDay"]')
+    expect(dayCell).toHaveAttribute('aria-selected', 'false')
+
+    rerender(
+      <Day
+        date="2019-08-02"
+        label="1 August 2019 Friday"
+        role="gridcell"
+        isSelected={true}
+      >
+        8
+      </Day>
+    )
+    const dayCellSelected = container.querySelector('[class$="-calendarDay"]')
+    expect(dayCellSelected).toHaveAttribute('aria-selected', 'true')
+  })
+
+  it('should call onClick with date', async () => {
+    const onClick = vi.fn()
+    const date = '2019-08-02'
+
+    const { container } = render(
+      <Day date={date} label="1 August 2019 Friday" onClick={onClick}>
+        8
+      </Day>
+    )
+    const day = container.querySelector('[class$="-calendarDay"]')
+
+    await userEvent.click(day!)
+
+    await waitFor(() => {
+      const args = onClick.mock.calls[0][1]
+
+      expect(onClick).toHaveBeenCalledTimes(1)
+      expect(args).toHaveProperty('date', date)
+    })
+  })
+
+  it('should call onKeyDown with date', async () => {
+    const onKeyDown = vi.fn()
+    const date = '2019-08-02'
+
+    const { container } = render(
+      <Day
+        date={date}
+        label="1 August 2019 Friday"
+        onKeyDown={onKeyDown}
+        onClick={() => {}}
+      >
+        8
+      </Day>
+    )
+
+    const day = container.querySelector('[class$="-calendarDay"]')!
+
+    userEvent.type(day, '{enter}')
+
+    await waitFor(() => {
+      const args = onKeyDown.mock.calls[0][1]
+
+      expect(onKeyDown).toHaveBeenCalledTimes(1)
+      expect(args).toHaveProperty('date', date)
+    })
+  })
+
+  it('should apply disabled when interaction is `disabled`', async () => {
+    const onClick = vi.fn()
+
+    const { container } = render(
+      <Day
+        date="2019-08-02"
+        label="1 August 2019 Friday"
+        onClick={onClick}
+        interaction="disabled"
+      >
+        8
+      </Day>
+    )
+    const day = container.querySelector('[class$="-calendarDay"]')!
+
+    userEvent.click(day)
+
+    await waitFor(() => {
+      expect(onClick).not.toHaveBeenCalled()
+      expect(day).toHaveAttribute('disabled')
+    })
+  })
+
+  it('should provide an elementRef', async () => {
+    let element
+    const elementRef = (el: Element | null) => {
+      element = el
+    }
+    const { container } = render(
+      <Day
+        date="2019-08-02"
+        label="1 August 2019 Friday"
+        elementRef={elementRef}
+      >
+        8
+      </Day>
+    )
+    const day = container.querySelector('[class$="-calendarDay"]')
+
+    expect(day).toEqual(element)
+  })
+
+  describe('element type', () => {
+    it('should render as a span by default', async () => {
+      const { container } = render(
+        <Day date="2019-08-02" label="1 August 2019 Friday">
+          8
+        </Day>
+      )
+      const day = container.querySelector('[class$="-calendarDay"]')
+
+      expect(day?.tagName).toEqual('SPAN')
+    })
+
+    it('should render as a button when onClick is provided', async () => {
+      const { container } = render(
+        <Day date="2019-08-02" label="1 August 2019 Friday" onClick={() => {}}>
+          8
+        </Day>
+      )
+      const day = container.querySelector('[class$="-calendarDay"]')
+
+      expect(day?.tagName).toEqual('BUTTON')
+    })
+
+    it('default elementTypes should be overwritten when `as` prop is set', async () => {
+      const { container } = render(
+        <Day
+          date="2019-08-02"
+          label="1 August 2019 Friday"
+          onClick={() => {}}
+          as="li"
+        >
+          8
+        </Day>
+      )
+      const day = container.querySelector('[class$="-calendarDay"]')
+
+      expect(day?.tagName).toEqual('LI')
+    })
+  })
+
+  describe('with generated examples', () => {
+    const generatedComponents = generateA11yTests(Day, DayExamples)
+
+    for (const component of generatedComponents) {
+      it(component.description, async () => {
+        const { container } = render(component.content)
+        const axeCheck = await runAxeCheck(container)
+        expect(axeCheck).toBe(true)
+      })
+    }
+  })
+})
--- packages/ui-calendar/src/Calendar/__new-tests__/Calendar.test.tsx
+++ packages/ui-calendar/src/Calendar/__new-tests__/Calendar.test.tsx
@@ -22,11 +22,38 @@
  * SOFTWARE.
  */
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { vi } from 'vitest'
-
 import '@testing-library/jest-dom'
+
+// eslint-disable-next-line no-restricted-imports
+import { generateA11yTests } from '@instructure/ui-scripts/lib/test/generateA11yTests'
+import { runAxeCheck } from '@instructure/ui-axe-check'
+import CalendarExamples from '../__examples__/Calendar.examples'
+import type { CalendarDayProps } from '../Day/props'
 import Calendar from '../index'
+
+const weekdayLabels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+
+const generateDays = (count = Calendar.DAY_COUNT) => {
+  const days: React.ReactElement<CalendarDayProps>[] = []
+  const date = new Date('2019-07-28')
+  while (days.length < count) {
+    days.push(
+      <Calendar.Day
+        key={date.toISOString()}
+        date={date.toISOString()}
+        label={date.toISOString()}
+        isOutsideMonth={date.getMonth() !== 7}
+      >
+        {date.getDate()}
+      </Calendar.Day>
+    )
+    date.setDate(date.getDate() + 1)
+  }
+  return days
+}
 
 describe('<Calendar />', () => {
   let consoleWarningMock: ReturnType<typeof vi.spyOn>
@@ -51,22 +78,28 @@ describe('<Calendar />', () => {
     it('should render 44 buttons without config', async () => {
       render(<Calendar />)
       const buttons = document.getElementsByTagName('button')
+
       expect(buttons.length).toEqual(44)
     })
+
     it('should render proper week names by default', async () => {
       const { container } = render(<Calendar />)
       const thead = container.querySelector('thead')
+
       expect(thead).toHaveTextContent(
         'SundaySuMondayMoTuesdayTuWednesdayWeThursdayThFridayFrSaturdaySa'
       )
     })
+
     it('should render proper week names if locale is set', async () => {
       const { container } = render(<Calendar locale="hu" />)
       const thead = container.querySelector('thead')
+
       expect(thead).toHaveTextContent(
         'hétfőhkeddkszerdaszecsütörtökcspéntekpszombatszovasárnapv'
       )
     })
+
     it('should disable days properly', async () => {
       const { container } = render(
         <Calendar
@@ -85,6 +118,7 @@ describe('<Calendar />', () => {
         expect(button).toHaveTextContent(disabledTexts[index])
       })
     })
+
     it('should indicate selected day', async () => {
       render(<Calendar currentDate="2023-12-15" selectedDate="2023-12-22" />)
 
@@ -98,5 +132,285 @@ describe('<Calendar />', () => {
         )
       }
     })
+  })
+
+  it('should render children', async () => {
+    const { container } = render(
+      <Calendar renderWeekdayLabels={weekdayLabels}>{generateDays()}</Calendar>
+    )
+
+    const calendarDays = container.querySelectorAll(
+      'span[class$="-calendarDay__day"]'
+    )
+
+    expect(calendarDays.length).toEqual(Calendar.DAY_COUNT)
+  })
+
+  it(`should warn if the correct number of children are not provided`, async () => {
+    const count = Calendar.DAY_COUNT - 1
+
+    render(
+      <Calendar renderWeekdayLabels={weekdayLabels}>
+        {generateDays(count)}
+      </Calendar>
+    )
+
+    const expectedErrorMessage = `should have exactly ${Calendar.DAY_COUNT} children. ${count} provided.`
+
+    expect(consoleErrorMock).toHaveBeenCalledWith(
+      expect.stringContaining(expectedErrorMessage),
+      expect.any(String)
+    )
+  })
+
+  it('should render weekday labels', async () => {
+    const { container, rerender } = render(
+      <Calendar renderWeekdayLabels={weekdayLabels}>{generateDays()}</Calendar>
+    )
+
+    const originalHeaders = container.querySelectorAll('th')
+    expect(originalHeaders.length).toEqual(7)
+
+    weekdayLabels.forEach((label, i) => {
+      expect(originalHeaders[i]).toHaveTextContent(label)
+    })
+
+    const functionalWeekdayLabels = [
+      () => 'Sx',
+      () => 'Mx',
+      () => 'Tx',
+      () => 'Wx',
+      () => 'Tx',
+      () => 'Fx',
+      () => 'Sx'
+    ]
+
+    // Set prop: renderWeekdayLabels
+    rerender(
+      <Calendar renderWeekdayLabels={functionalWeekdayLabels}>
+        {generateDays()}
+      </Calendar>
+    )
+
+    const updatedHeaders = container.querySelectorAll('th')
+    expect(updatedHeaders.length).toEqual(7)
+
+    functionalWeekdayLabels.forEach((functionalLabel, i) => {
+      expect(updatedHeaders[i]).toHaveTextContent(functionalLabel())
+    })
+  })
+
+  it('should warn if 7 weekday labels are not provided', async () => {
+    render(<Calendar renderWeekdayLabels={[]}>{generateDays()}</Calendar>)
+
+    const expectedErrorMessage =
+      '`renderWeekdayLabels` should be an array with 7 labels (one for each weekday). 0 provided.'
+
+    expect(consoleErrorMock).toHaveBeenCalledWith(
+      expect.stringContaining(expectedErrorMessage),
+      expect.any(String)
+    )
+  })
+
+  it('should format the weekday labels and days correctly', async () => {
+    const { container } = render(
+      <Calendar renderWeekdayLabels={weekdayLabels}>{generateDays()}</Calendar>
+    )
+
+    const headerRow = container.querySelectorAll('thead > tr')
+    expect(headerRow.length).toEqual(1)
+
+    const headers = container.querySelectorAll('thead > tr > th')
+    expect(headers.length).toEqual(7)
+
+    const bodyRows = container.querySelectorAll('tbody > tr')
+    expect(bodyRows.length).toEqual(Calendar.DAY_COUNT / 7)
+
+    for (const row of bodyRows) {
+      const days = row.querySelectorAll('td')
+      expect(days.length).toEqual(7)
+    }
+  })
+
+  it('should render navigation label', async () => {
+    const navLabel = (
+      <span>
+        <div>March</div>
+        <div>2019</div>
+      </span>
+    )
+
+    const { rerender } = render(
+      <Calendar
+        renderWeekdayLabels={weekdayLabels}
+        renderNavigationLabel={navLabel}
+      >
+        {generateDays()}
+      </Calendar>
+    )
+    const month = screen.getByText('March')
+    const year = screen.getByText('2019')
+
+    expect(month).toBeInTheDocument()
+    expect(year).toBeInTheDocument()
+
+    // Set prop: renderNavigationLabel
+    rerender(
+      <Calendar
+        renderWeekdayLabels={weekdayLabels}
+        renderNavigationLabel={() => navLabel}
+      >
+        {generateDays()}
+      </Calendar>
+    )
+    const updatedMonth = screen.getByText('March')
+    const updatedYear = screen.getByText('2019')
+
+    expect(updatedMonth).toBeInTheDocument()
+    expect(updatedYear).toBeInTheDocument()
+  })
+
+  it('should render next and prev buttons', async () => {
+    const { rerender } = render(
+      <Calendar
+        renderWeekdayLabels={weekdayLabels}
+      >
+        {generateDays()}
+      </Calendar>
+    )
+    const defaultPrevButton = screen.getByText('Previous month')
+    const defaultNextButton = screen.getByText('Next month')
+
+    expect(defaultPrevButton).toBeInTheDocument()
+    expect(defaultNextButton).toBeInTheDocument()
+
+    rerender(
+      <Calendar
+        renderWeekdayLabels={weekdayLabels}
+        renderPrevMonthButton={<button>test-prev</button>}
+        renderNextMonthButton={<button>test-next</button>}
+      >
+        {generateDays()}
+      </Calendar>
+    )
+    const customPrevButton = screen.getByText('test-prev')
+    const customNextButton = screen.getByText('test-next')
+
+    expect(customPrevButton).toBeInTheDocument()
+    expect(customNextButton).toBeInTheDocument()
+
+    rerender(
+      <Calendar
+        renderWeekdayLabels={weekdayLabels}
+        renderPrevMonthButton={() => <button>func-test-prev</button>}
+        renderNextMonthButton={() => <button>func-test-next</button>}
+      >
+        {generateDays()}
+      </Calendar>
+    )
+    const funcPrevButton = screen.getByText('func-test-prev')
+    const funcNextButton = screen.getByText('func-test-next')
+
+    expect(funcPrevButton).toBeInTheDocument()
+    expect(funcNextButton).toBeInTheDocument()
+  })
+
+  it('should call onRequestRenderNextMonth and onRequestRenderPrevMonth', async () => {
+    const onRequestRenderPrevMonth = vi.fn()
+    const onRequestRenderNextMonth = vi.fn()
+
+    render(
+      <Calendar
+        renderWeekdayLabels={weekdayLabels}
+        renderPrevMonthButton={<button>prev month</button>}
+        renderNextMonthButton={<button>next month</button>}
+        onRequestRenderPrevMonth={onRequestRenderPrevMonth}
+        onRequestRenderNextMonth={onRequestRenderNextMonth}
+      >
+        {generateDays()}
+      </Calendar>
+    )
+
+    const prevButton = screen.getByText('prev month')
+    const nextButton = screen.getByText('next month')
+
+    expect(onRequestRenderPrevMonth).not.toHaveBeenCalled()
+    expect(onRequestRenderNextMonth).not.toHaveBeenCalled()
+
+    await userEvent.click(prevButton)
+    await userEvent.click(nextButton)
+
+    await waitFor(() => {
+      expect(onRequestRenderPrevMonth).toHaveBeenCalledTimes(1)
+      expect(onRequestRenderNextMonth).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  describe('when role="listbox"', () => {
+    it('should set role="listbox" on table root and role="presentation" on the correct elements', async () => {
+      const { container } = render(
+        <Calendar renderWeekdayLabels={weekdayLabels} role="listbox">
+          {generateDays()}
+        </Calendar>
+      )
+
+      const table = container.querySelector('table')
+      expect(table).toHaveAttribute('role', 'listbox')
+
+      const rows = container.querySelectorAll('tbody > tr')
+      for (const row of rows) {
+        expect(row).toHaveAttribute('role', 'presentation')
+
+        const tds = row.querySelectorAll('td')
+        tds.forEach((td) => {
+          expect(td).toHaveAttribute('role', 'presentation')
+        })
+      }
+    })
+
+    it("should link each day with it's weekday header via `aria-describedby`", async () => {
+      const { container } = render(
+        <Calendar renderWeekdayLabels={weekdayLabels} role="listbox">
+          {generateDays()}
+        </Calendar>
+      )
+
+      const days = container.querySelectorAll('tbody > tr > td')
+      const headers = container.querySelectorAll('th')
+
+      days.forEach((dayTd, i) => {
+        const index = i % 7
+        const day = dayTd.querySelector('span[class$="-calendarDay"]')
+
+        expect(day).toHaveAttribute(
+          'aria-describedby',
+          headers[index].getAttribute('id')
+        )
+      })
+    })
+  })
+
+  it('should render root as designated by the `as` prop', async () => {
+    render(
+      <Calendar renderWeekdayLabels={weekdayLabels} as="ul">
+        {generateDays()}
+      </Calendar>
+    )
+    const calendar = screen.getByRole('list')
+
+    expect(calendar.tagName).toBe('UL')
+    expect(calendar).toHaveTextContent(weekdayLabels.join(''))
+  })
+
+  describe('with generated examples', () => {
+    const generatedComponents = generateA11yTests(Calendar, CalendarExamples)
+
+    for (const component of generatedComponents) {
+      it(component.description, async () => {
+        const { container } = render(component.content)
+        const axeCheck = await runAxeCheck(container)
+        expect(axeCheck).toBe(true)
+      })
+    }
   })
 })
