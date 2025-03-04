263211d3e019ea52a13389f1b3c200f8b03f31b0
Nandor_Czegledi <nandor.czegledi@instructure.com>
2025-03-04 11:32:10 +0100

    test(ui-toggle-details): migrate old ToggleDetails tests

--- packages/ui-toggle-details/src/ToggleGroup/__new-tests__/ToggleGroup.test.tsx
+++ packages/ui-toggle-details/src/ToggleGroup/__new-tests__/ToggleGroup.test.tsx
@@ -23,15 +23,38 @@
  */
 
 import React from 'react'
-import { expect, mount, spy, wait } from '@instructure/ui-test-utils'
+import { render, screen, waitFor } from '@testing-library/react'
+import { runAxeCheck } from '@instructure/ui-axe-check'
+import userEvent from '@testing-library/user-event'
+import { vi } from 'vitest'
+import type { MockInstance } from 'vitest'
 
+import '@testing-library/jest-dom'
 import { ToggleGroup } from '../index'
-import { ToggleGroupLocator } from '../ToggleGroupLocator'
 
 describe('<ToggleGroup />', () => {
+  let consoleWarningMock: ReturnType<typeof vi.spyOn>
+  let consoleErrorMock: ReturnType<typeof vi.spyOn>
+
+  beforeEach(() => {
+    // Mocking console to prevent test output pollution
+    consoleWarningMock = vi
+      .spyOn(console, 'warn')
+      .mockImplementation(() => {}) as MockInstance
+    consoleErrorMock = vi
+      .spyOn(console, 'error')
+      .mockImplementation(() => {}) as MockInstance
+  })
+
+  afterEach(() => {
+    consoleWarningMock.mockRestore()
+    consoleErrorMock.mockRestore()
+  })
+
   it('should show its summary and hide its children by default', async () => {
-    await mount(
+    render(
       <ToggleGroup
+        data-testId="toggle-group"
         transition={false}
         summary="This is the summary section"
         toggleLabel="This is the toggleLabel"
@@ -39,20 +62,16 @@ describe('<ToggleGroup />', () => {
         This is the details section
       </ToggleGroup>
     )
+    const toggleGroup = screen.getByTestId('toggle-group')
 
-    const toggleGroup = await ToggleGroupLocator.find()
-
-    expect(toggleGroup.getTextContent()).to.contain(
-      'This is the summary section'
-    )
-    expect(toggleGroup.getTextContent()).not.to.contain(
-      'This is the details section'
-    )
+    expect(toggleGroup).toHaveTextContent('This is the summary section')
+    expect(toggleGroup).not.toHaveTextContent('This is the details section')
   })
 
   it('should render with children showing with the defaultExpanded prop', async () => {
-    await mount(
+    const { container } = render(
       <ToggleGroup
+        data-testId="toggle-group"
         transition={false}
         summary="This is the summary section"
         toggleLabel="This is the toggleLabel"
@@ -61,17 +80,15 @@ describe('<ToggleGroup />', () => {
         This is the details section
       </ToggleGroup>
     )
-    const toggleGroup = await ToggleGroupLocator.find()
-    const toggle = await toggleGroup.findToggle()
+    const toggleGroup = screen.getByTestId('toggle-group')
+    const toggle = container.querySelector('button')
 
-    expect(toggle.getAttribute('aria-expanded')).to.equal('true')
-    expect(toggleGroup.getTextContent()).to.contain(
-      'This is the details section'
-    )
+    expect(toggle).toHaveAttribute('aria-expanded', 'true')
+    expect(toggleGroup).toHaveTextContent('This is the details section')
   })
 
   it('should have an aria-controls attribute', async () => {
-    await mount(
+    const { container } = render(
       <ToggleGroup
         transition={false}
         summary="This is the summary section"
@@ -81,18 +98,14 @@ describe('<ToggleGroup />', () => {
         This is the details section
       </ToggleGroup>
     )
+    const content = screen.getByText('This is the details section')
+    const toggle = container.querySelector('button')
 
-    const toggleGroup = await ToggleGroupLocator.find()
-    const toggle = await toggleGroup.findToggle()
-    const content = await toggleGroup.findContent()
-
-    expect(toggle.getAttribute('aria-controls')).to.equal(
-      content!.getAttribute('id')
-    )
+    expect(toggle).toHaveAttribute('aria-controls', content.id)
   })
 
   it('should have an aria-expanded attribute', async () => {
-    await mount(
+    const { container } = render(
       <ToggleGroup
         transition={false}
         summary="This is the summary section"
@@ -101,15 +114,13 @@ describe('<ToggleGroup />', () => {
         This is the details section
       </ToggleGroup>
     )
+    const toggle = container.querySelector('button')
 
-    const toggleGroup = await ToggleGroupLocator.find()
-    const toggle = await toggleGroup.findToggle()
-
-    expect(toggle.getAttribute('aria-expanded')).to.equal('false')
+    expect(toggle).toHaveAttribute('aria-expanded', 'false')
   })
 
   it('should toggle on click events', async () => {
-    await mount(
+    const { container } = render(
       <ToggleGroup
         transition={false}
         summary="This is the summary section"
@@ -118,21 +129,21 @@ describe('<ToggleGroup />', () => {
         This is the details section
       </ToggleGroup>
     )
+    const toggle = container.querySelector('button')!
 
-    const toggleGroup = await ToggleGroupLocator.find()
-    await toggleGroup.clickToggle()
+    expect(toggle).toHaveAttribute('aria-expanded', 'false')
 
-    const toggle = await toggleGroup.findToggle()
+    await userEvent.click(toggle)
 
-    expect(toggle.getAttribute('aria-expanded')).to.equal('true')
+    await waitFor(() => {
+      expect(toggle).toHaveAttribute('aria-expanded', 'true')
+    })
   })
 
   it('should call onToggle on click events', async () => {
-    const onToggle = spy((e: React.MouseEvent) => {
-      e.persist()
-    })
+    const onToggle = vi.fn()
 
-    await mount(
+    const { container } = render(
       <ToggleGroup
         transition={false}
         summary="This is the summary section"
@@ -143,17 +154,21 @@ describe('<ToggleGroup />', () => {
         This is the details section
       </ToggleGroup>
     )
-    const toggleGroup = await ToggleGroupLocator.find()
-    await toggleGroup.clickToggle()
+    const toggle = container.querySelector('button')!
 
-    const { args } = onToggle.firstCall
+    await userEvent.click(toggle)
 
-    expect((args[0] as any).type).to.equal('click')
-    expect(args[1]).to.equal(true)
+    await waitFor(() => {
+      const args = onToggle.mock.calls[0]
+
+      expect(onToggle).toHaveBeenCalledTimes(1)
+      expect(args[0].type).toBe('click')
+      expect(args[1]).toBe(true)
+    })
   })
 
   it('should update the toggle screenreader label based on the expanded state', async () => {
-    await mount(
+    const { container } = render(
       <ToggleGroup
         transition={false}
         summary="This is the summary section"
@@ -162,14 +177,18 @@ describe('<ToggleGroup />', () => {
         This is the details section
       </ToggleGroup>
     )
-    const toggleGroup = await ToggleGroupLocator.find()
-    const toggle = await toggleGroup.findToggle()
+    const toggle = container.querySelector('button')!
+    const scrContent = container.querySelector(
+      '[class$="-screenReaderContent"]'
+    )
 
-    expect(toggle.getTextContent()).to.equal('Show content')
+    expect(scrContent).toHaveTextContent('Show content')
 
-    await toggleGroup.clickToggle()
+    await userEvent.click(toggle)
 
-    expect(toggle.getTextContent()).to.equal('Hide content')
+    await waitFor(() => {
+      expect(scrContent).toHaveTextContent('Hide content')
+    })
   })
 
   it('should accept custom icons', async () => {
@@ -187,7 +206,7 @@ describe('<ToggleGroup />', () => {
       </svg>
     )
 
-    await mount(
+    const { container } = render(
       <ToggleGroup
         transition={false}
         summary="This is the summary section"
@@ -198,26 +217,20 @@ describe('<ToggleGroup />', () => {
         This is the details section
       </ToggleGroup>
     )
+    const toggle = container.querySelector('button')!
+    const svg = container.querySelector('svg')!
 
-    const toggleGroup = await ToggleGroupLocator.find()
+    expect(svg).toHaveTextContent('Icon collapsed')
 
-    let iconTitle
+    await userEvent.click(toggle)
 
-    await wait(async () => {
-      iconTitle = await toggleGroup.find('svg > title')
-      expect(iconTitle.getTextContent()).to.equal('Icon collapsed')
-    })
-
-    await toggleGroup.clickToggle()
-
-    await wait(async () => {
-      iconTitle = await toggleGroup.find('svg > title')
-      expect(iconTitle.getTextContent()).to.equal('Icon expanded')
+    await waitFor(() => {
+      expect(svg).toHaveTextContent('Icon expanded')
     })
   })
 
   it('should meet a11y standards', async () => {
-    await mount(
+    const { container } = render(
       <ToggleGroup
         transition={false}
         summary="This is the summary section"
@@ -226,32 +239,32 @@ describe('<ToggleGroup />', () => {
         This is the details section
       </ToggleGroup>
     )
+    const axeCheck = await runAxeCheck(container)
 
-    const toggleGroup = await ToggleGroupLocator.find()
-    expect(await toggleGroup.accessible()).to.be.true()
+    expect(axeCheck).toBe(true)
   })
 
   it('focuses with the focus helper', async () => {
-    let toggleRef: ToggleGroup | undefined
-    await mount(
+    let toggleRef: any
+    const { container } = render(
       <ToggleGroup
+        data-testId="toggle-group"
         transition={false}
         summary="This is the summary section"
         toggleLabel="This is the toggleLabel"
-        //@ts-expect-error TODO this is coming from ReactComponentWrapper
-        componentRef={(el) => {
+        ref={(el) => {
           toggleRef = el
         }}
       >
         This is the details section
       </ToggleGroup>
     )
+    const toggle = container.querySelector('button')!
 
-    const toggleGroup = await ToggleGroupLocator.find()
-    const toggle = await toggleGroup.findToggle()
-    expect(toggleRef?.focused).to.be.false()
+    expect(document.activeElement).not.toBe(toggle)
+
     toggleRef?.focus()
-    expect(toggleRef?.focused).to.be.true()
-    expect(toggle.getDOMNode()).to.equal(document.activeElement)
+
+    expect(document.activeElement).toBe(toggle)
   })
 })
