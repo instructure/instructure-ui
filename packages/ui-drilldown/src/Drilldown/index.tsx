9545953ba963f7c1470e89f02a79a65b7d5ae6ef
Peter Pal Hudak <hudakpe@gmail.com>
2025-03-19 13:52:09 +0100

    feat(ui-top-nav-bar,ui-drilldown): adjust types

--- packages/ui-drilldown/src/Drilldown/index.tsx
+++ packages/ui-drilldown/src/Drilldown/index.tsx
@@ -427,7 +427,7 @@ class Drilldown extends Component<DrilldownProps, DrilldownState> {
     const { currentPage } = this
     const { styles, deterministicId } = this.props
 
-    const headerChildren: PageChildren[] = []
+    const headerChildren: (PageChildren | React.ReactNode)[] = []
 
     if (!currentPage) {
       return headerChildren
@@ -945,7 +945,7 @@ class Drilldown extends Component<DrilldownProps, DrilldownState> {
       return null
     }
 
-    const pageChildren: PageChildren[] = [
+    const pageChildren: (PageChildren | React.ReactNode)[] = [
       ...headerChildren,
       ...currentPage.children
     ]
