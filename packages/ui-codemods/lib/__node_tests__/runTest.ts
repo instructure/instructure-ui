e5323913790faf6b955ef40fde77baa040f950fc
Nandor_Czegledi <nandor.czegledi@instructure.com>
2025-07-17 17:58:52 +0200

    Solve Property 'path' error in codemods runTests.ts

--- packages/ui-codemods/lib/__node_tests__/runTest.ts
+++ packages/ui-codemods/lib/__node_tests__/runTest.ts
@@ -39,7 +39,8 @@ export function runTest(codemod: Transform) {
   const entries = fs.readdirSync(
     `${__dirname}/__testfixtures__/${codemod.name}`,
     { withFileTypes: true }
-  )
+  ) as Array<fs.Dirent & { path: string }>
+
   let fixturesRun = 0
   entries.forEach((entry) => {
     if (entry.isFile() && entry.name.indexOf('input') > -1) {
