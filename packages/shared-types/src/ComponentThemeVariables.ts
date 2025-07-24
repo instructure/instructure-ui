e85706a4ff2c4795a3cc8e031efb2cf059ee7394
Nandor_Czegledi <nandor.czegledi@instructure.com>
2025-07-24 18:37:10 +0200

    Remove maxWidth from tag

--- packages/shared-types/src/ComponentThemeVariables.ts
+++ packages/shared-types/src/ComponentThemeVariables.ts
@@ -1425,7 +1425,6 @@ export type TagTheme = {
   focusOutlineColor: Colors['contrasts']['blue4570']
   focusOutlineWidth: Border['widthMedium']
   focusOutlineStyle: Border['style']
-  maxWidth: string | 0
   iconMargin: Spacing['small']
   transitionTiming: string
   defaultBackgroundHover: Colors['contrasts']['grey1214']
