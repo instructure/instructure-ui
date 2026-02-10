64ce54e282313507c868ceefb1b002e29b672f4d
Nandor_Czegledi <nandor.czegledi@instructure.com>
2026-02-10 10:15:43 +0100

    feat(ui-pagination): migrate to new theming system

--- packages/ui-pagination/src/Pagination/PaginationPageInput/styles.ts
+++ packages/ui-pagination/src/Pagination/PaginationPageInput/styles.ts
@@ -22,7 +22,7 @@
  * SOFTWARE.
  */
 
-import type { PaginationPageInputTheme } from '@instructure/shared-types'
+import type { NewComponentTypes } from '@instructure/ui-themes'
 import type { PaginationPageInputStyle } from './props'
 
 /**
@@ -32,11 +32,12 @@ import type { PaginationPageInputStyle } from './props'
  * Generates the style object from the theme and provided additional information
  * @param  {Object} componentTheme The theme variable object.
  * @param  {Object} props the props of the component, the style is applied to
+ * @param  {Object} sharedTokens Shared token object that stores common values for the theme.
  * @param  {Object} state the state of the component, the style is applied to
  * @return {Object} The final style object, which will be used in the component
  */
 const generateStyle = (
-  componentTheme: PaginationPageInputTheme
+  componentTheme: NewComponentTypes['PaginationPageInput']
 ): PaginationPageInputStyle => {
   return {
     paginationPageInput: {
