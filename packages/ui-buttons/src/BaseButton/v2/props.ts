1fb0ff582dfa7fc1e15aef16abd35b7633f9692f
Balázs Sáros <balazs.saros@instructure.com>
2026-03-13 15:57:55 +0100

    feat(ui-buttons): add v2 button components with new icon system and theming

---
+++ packages/ui-buttons/src/BaseButton/v2/props.ts
@@ -0,0 +1,226 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 - present Instructure, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import React from 'react'
+import type { InteractionType } from '@instructure/ui-react-utils'
+import type {
+  Spacing,
+  WithStyleProps,
+  ComponentStyle
+} from '@instructure/emotion'
+import type {
+  ToProp,
+  AsElementType,
+  BaseButtonTheme,
+  OtherHTMLAttributes
+} from '@instructure/shared-types'
+import type { Cursor } from '@instructure/shared-types'
+import type { ViewProps } from '@instructure/ui-view/latest'
+import { Renderable } from '@instructure/shared-types'
+
+type BaseButtonOwnProps = {
+  /**
+   * Specifies the `Button` children.
+   */
+  children?: React.ReactNode
+
+  /**
+   * Specifies the type of the `Button`'s underlying html element.
+   */
+  type?: 'button' | 'submit' | 'reset'
+
+  /**
+   * The size of the `Button`
+   */
+  size?: 'small' | 'medium' | 'large' | 'condensedSmall' | 'condensedMedium'
+
+  /**
+   * Provides a reference to the `Button`'s underlying html element.
+   */
+  elementRef?: (element: HTMLElement | null) => void
+
+  /**
+   * The element to render as the component root, `Button` by default.
+   */
+  as?: AsElementType
+
+  /**
+   * Specifies if interaction with the `Button` is enabled, disabled, or readonly.
+   */
+  interaction?: InteractionType
+
+  /**
+   * Specifies the color for the `Button`.
+   */
+  color?:
+    | 'primary'
+    | 'primary-inverse'
+    | 'secondary'
+    | 'success'
+    | 'danger'
+    | 'ai-primary'
+    | 'ai-secondary'
+
+  /**
+   * Override the `Button`'s default focus outline color.
+   */
+  focusColor?: 'info' | 'inverse'
+
+  /**
+   * The `Button` display property. When set to `inline-block`, the `Button` displays inline with other elements.
+   * When set to block, the `Button` expands to fill the width of the container.
+   */
+  display?: 'inline-block' | 'block'
+
+  /**
+   * Sets the alignment of the `Button` children and/or icon.
+   */
+  textAlign?: 'start' | 'center'
+
+  /**
+   * Specifies if the `Button` shape should be a circle or rectangle.
+   */
+  shape?: 'rectangle' | 'circle'
+
+  /**
+   * Specifies if the `Button` should render with a solid background. When false, the background is transparent.
+   */
+  withBackground?: boolean
+
+  /**
+   * Specifies if the `Button` should render with a border.
+   */
+  withBorder?: boolean
+
+  /**
+   * Designates if the `Button` should render without padding. This option should only be set when `withBorder` and
+   * `withBackground` are also set to false.
+   */
+  isCondensed?: boolean
+
+  /**
+   * Valid values are `0`, `none`, `auto`, `xxx-small`, `xx-small`, `x-small`,
+   * `small`, `medium`, `large`, `x-large`, `xx-large`. Apply these values via
+   * familiar CSS-like shorthand. For example: `margin="small auto large"`.
+   */
+  margin?: Spacing
+
+  /**
+   * Specify a mouse cursor to use when hovering over the button.
+   * The `pointer` cursor is used by default.
+   */
+  cursor?: Cursor
+
+  /**
+   * Specifies an href attribute for the `Button`'s underlying html element.
+   */
+  href?: string
+
+  /**
+   * Callback fired when the `Button` is clicked.
+   */
+  onClick?: (
+    event: React.KeyboardEvent<ViewProps> | React.MouseEvent<ViewProps>
+  ) => void
+
+  /**
+   * Callback fired when the `Button` receives a keydown event.
+   */
+  onKeyDown?: (event: React.KeyboardEvent<ViewProps>) => void
+
+  /**
+   * An icon, or function that returns an icon.
+   */
+  renderIcon?: Renderable
+
+  /**
+   * Specifies the tabindex of the `Button`.
+   */
+  tabIndex?: number
+
+  /**
+   * Manually control if the `Button` should display a focus outline.
+   *
+   * When left `undefined` (which is the default) the focus outline will display
+   * if this component is focusable and receives focus.
+   */
+  withFocusOutline?: boolean
+}
+
+type BaseButtonStyleProps = {
+  isDisabled: boolean
+  hasOnlyIconVisible: boolean
+  isEnabled: boolean
+}
+
+type PropKeys = keyof BaseButtonOwnProps
+
+type AllowedPropKeys = Readonly<Array<PropKeys>>
+
+type BaseButtonProps = BaseButtonOwnProps &
+  WithStyleProps<BaseButtonTheme, BaseButtonStyle> &
+  OtherHTMLAttributes<BaseButtonOwnProps> &
+  ToProp
+
+type BaseButtonStyle = ComponentStyle<
+  | 'baseButton'
+  | 'content'
+  | 'children'
+  | 'iconSVG'
+  | 'childrenLayout'
+  | 'iconOnly'
+  | 'iconWrapper'
+  | 'childrenWrapper'
+>
+const allowedProps: AllowedPropKeys = [
+  'as',
+  'children',
+  'color',
+  'cursor',
+  'display',
+  'elementRef',
+  'focusColor',
+  'href',
+  'interaction',
+  'isCondensed',
+  'margin',
+  'onClick',
+  'onKeyDown',
+  'renderIcon',
+  'shape',
+  'size',
+  'tabIndex',
+  'textAlign',
+  'type',
+  'withBackground',
+  'withBorder'
+]
+
+export type {
+  BaseButtonProps,
+  BaseButtonOwnProps,
+  BaseButtonStyleProps,
+  BaseButtonStyle
+}
+export { allowedProps }
---
+++ packages/ui-buttons/src/ToggleButton/v2/props.ts
@@ -0,0 +1,143 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 - present Instructure, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import React from 'react'
+
+import type {
+  AsElementType,
+  OtherHTMLAttributes
+} from '@instructure/shared-types'
+import type {
+  PlacementPropValues,
+  PositionConstraint,
+  PositionMountNode
+} from '@instructure/ui-position'
+import type { ViewProps } from '@instructure/ui-view/latest'
+import { Renderable } from '@instructure/shared-types'
+
+type ToggleButtonOwnProps = {
+  /**
+   * Text to output only to screen readers
+   */
+  screenReaderLabel: string
+
+  /**
+   * Text to render in the tooltip shown on hover/focus
+   */
+  renderTooltipContent: React.ReactNode | ((...args: any[]) => any)
+
+  /**
+   * An icon or function that returns an icon
+   */
+  renderIcon: Renderable
+
+  /**
+   * Toggles the `aria-pressed` attribute on the button (`true` if `pressed`; `false` if `unpressed`)
+   */
+  status: 'pressed' | 'unpressed'
+
+  /**
+   * The element to render as the component root; `button` by default
+   */
+  as?: AsElementType
+
+  /**
+   * Specifies if interaction with `ToggleButton` is `enabled`, `disabled`, or `readonly`
+   */
+  interaction?: 'enabled' | 'disabled' | 'readonly'
+
+  /**
+   * The size of the `ToggleButton`
+   */
+  size?: 'small' | 'medium' | 'large'
+
+  /**
+   * Provides a reference to `ToggleButton`'s underlying HTML element
+   */
+  elementRef?: (element: Element | null) => void
+
+  /**
+   * Callback fired when the `ToggleButton` is clicked
+   */
+  onClick?: (
+    event: React.KeyboardEvent<ViewProps> | React.MouseEvent<ViewProps>
+  ) => void
+
+  /**
+   * The color in which to display the icon
+   */
+  color?: 'primary' | 'primary-inverse' | 'secondary' | 'success' | 'danger'
+
+  /**
+   * By default, the tooltip will show on hover/focus. Use this prop if you need to override that behavior.
+   */
+  isShowingTooltip?: boolean
+
+  /**
+   * An element or a function returning an element to use as the mount node
+   */
+  mountNode?: PositionMountNode
+
+  /**
+   * The placement of the tooltip in relation to the button
+   */
+  placement?: PlacementPropValues
+
+  /**
+   * The parent in which to constrain the tooltip.
+   * One of: 'window', 'scroll-parent', 'parent', 'none', an element,
+   * or a function returning an element.
+   */
+  constrain?: PositionConstraint
+}
+
+type PropKeys = keyof ToggleButtonOwnProps
+
+type AllowedPropKeys = Readonly<Array<PropKeys>>
+
+type ToggleButtonProps = ToggleButtonOwnProps &
+  OtherHTMLAttributes<ToggleButtonOwnProps>
+
+type ToggleButtonState = {
+  isShowingTooltip: boolean
+}
+const allowedProps: AllowedPropKeys = [
+  'as',
+  'color',
+  'constrain',
+  'elementRef',
+  'interaction',
+  'isShowingTooltip',
+  'mountNode',
+  'onClick',
+  'placement',
+  'renderIcon',
+  'renderTooltipContent',
+  'screenReaderLabel',
+  'size',
+  'status'
+]
+
+export type { ToggleButtonProps, ToggleButtonState }
+export { allowedProps }
