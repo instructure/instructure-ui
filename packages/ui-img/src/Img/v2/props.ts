5e7a3556401ac3155a8ff58baad9359095f654ff
Tamas Kovacs <tamas.kovacs@instructure.com>
2026-04-07 10:21:18 +0200

    feat(ui-img): rework Img
    BREAKING CHANGE: contains breaking changes due to component using the new theming system
    
    INSTUI-4970

---
+++ packages/ui-img/src/Img/v2/props.ts
@@ -0,0 +1,89 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 - present Instructure, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import type { OtherHTMLAttributes } from '@instructure/shared-types'
+import type {
+  Spacing,
+  WithStyleProps,
+  ComponentStyle
+} from '@instructure/emotion'
+import type { NewComponentTypes } from '@instructure/ui-themes'
+
+type ImgOwnProps = {
+  src: string
+  alt?: string
+  display?: 'inline-block' | 'block'
+  /**
+   * Gets passed down to  the img component. Same as the native HTML img's loading attribute
+   */
+  loading?: 'eager' | 'lazy'
+  /**
+   * Valid values are `0`, `none`, `auto`, `xxx-small`, `xx-small`, `x-small`,
+   * `small`, `medium`, `large`, `x-large`, `xx-large`. Apply these values via
+   * familiar CSS-like shorthand. For example: `margin="small auto large"`.
+   */
+  margin?: Spacing
+  /**
+   * Valid values for `opacity` are `0` - `10`. Valid values for `blend` are
+   * `normal` (default), `multiply`, `screen`, `overlay`, and `color-burn`.
+   */
+  overlay?: {
+    color: string
+    opacity: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
+    blend?: 'normal' | 'multiply' | 'screen' | 'overlay' | 'color-burn'
+  }
+  withGrayscale?: boolean
+  withBlur?: boolean
+  constrain?: 'cover' | 'contain'
+  elementRef?: (element: Element | null) => void
+  height?: string | number
+  width?: string | number
+}
+
+type PropKeys = keyof ImgOwnProps
+
+type AllowedPropKeys = Readonly<Array<PropKeys>>
+
+type ImgProps = ImgOwnProps &
+  WithStyleProps<NewComponentTypes['Img'], ImgStyle> &
+  OtherHTMLAttributes<ImgOwnProps>
+
+type ImgStyle = ComponentStyle<'overlay' | 'container' | 'img'>
+const allowedProps: AllowedPropKeys = [
+  'src',
+  'alt',
+  'display',
+  'loading',
+  'margin',
+  'overlay',
+  'withGrayscale',
+  'withBlur',
+  'constrain',
+  'elementRef',
+  'height',
+  'width'
+]
+
+export type { ImgProps, ImgStyle }
+export { allowedProps }
