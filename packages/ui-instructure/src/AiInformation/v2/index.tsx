c30b823034bfeb354c1ce91c924b577f0dba6566
Peter Pal Hudak <hudakpe@gmail.com>
2026-06-25 13:14:50 +0200

    fix(ui-instructure): align AiInformation lower-section heading levels to h4
    INSTUI-5030

--- packages/ui-instructure/src/AiInformation/v2/index.tsx
+++ packages/ui-instructure/src/AiInformation/v2/index.tsx
@@ -160,7 +160,7 @@ const AiInformation = ({
                     />
                   </div>
                   <div css={styles?.modelNameText}>
-                    <Heading level="h3" variant="label">
+                    <Heading level="h4" variant="label">
                       {' '}
                       {modelNameText}{' '}
                     </Heading>
