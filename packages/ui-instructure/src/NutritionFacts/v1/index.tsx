b99dd570811b9d917bcdf6d4578a7764c830681e
Peter Pal Hudak <hudakpe@gmail.com>
2026-07-27 15:34:01 +0200

    fix(ui-instructure): use CondensedButton for AI component triggers

--- packages/ui-instructure/src/NutritionFacts/v1/index.tsx
+++ packages/ui-instructure/src/NutritionFacts/v1/index.tsx
@@ -23,10 +23,13 @@
  */
 import { useState } from 'react'
 import { Modal } from '@instructure/ui-modal/v11_6'
-import { Button, CloseButton } from '@instructure/ui-buttons/v11_6'
+import {
+  Button,
+  CloseButton,
+  CondensedButton
+} from '@instructure/ui-buttons/v11_6'
 import { Heading } from '@instructure/ui-heading/v11_6'
 import { Text } from '@instructure/ui-text/v11_6'
-import { Link } from '@instructure/ui-link/v11_6'
 import { useStyle as useStyleNew } from '@instructure/emotion'
 
 import { NutritionFactsProps } from './props.js'
@@ -59,17 +62,9 @@ const NutritionFacts = ({
 
   return (
     <div>
-      <Link
-        variant="standalone"
-        onClick={(e) => {
-          e.preventDefault()
-          setOpen(true)
-        }}
-        forceButtonRole={false}
-        href="#"
-      >
+      <CondensedButton onClick={() => setOpen(true)} aria-haspopup="dialog">
         {triggerText}
-      </Link>
+      </CondensedButton>
       <Modal
         open={open}
         onDismiss={() => {
